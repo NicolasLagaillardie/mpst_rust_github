22ed3402518a849c1e8d6779a81c8e69d891daf9
Lag <lagaillardie.nicolas@live.fr>
2020-10-26 14:08:24 +0100

    ChooseMpst fn done

--- src/macros/multiple/choose.rs
+++ src/macros/multiple/choose.rs
@@ -22,54 +22,47 @@ macro_rules! create_choose_type_multi {
     }
 }
 
-/// TODO
 #[macro_export]
-macro_rules! create_choose_mpst_session_multi {
+macro_rules! create_choose_mpst_session_multi_left {
     ($func_name:ident, $type_name: ident, $role:ident, $recv_func:ident, $name:ident, $sessionmpst_name:ident, $nsessions:literal, $exclusion:literal) => {
         mpst_seq::seq!(N in 1..$nsessions ! $exclusion {
-
-            // For left branch
-
             fn $func_name<'a, #(S#N:0,)10:0 #(R#N:0,)11:0>(
                 s: $sessionmpst_name<
-
-
-
                     #( // i in 1..K
                         $type_name<
                             ~( // j in 0..K
-                                S~N:2, // S(i + j) (with Dual if needed)
+                                <S~N:2 as mpstthree::binary::Session>::Dual,
                             )(
-                                <S~N:2 as Session>::Dual,
+                                S~N:2, // S(i + j) (with Dual if needed)
                             )0*
                             ~( // j in 0..K
-                                S~N:3, // S(diff * (diff + 1) / 2 + K + i + j) (with Dual if needed)
-                            )(
                                 <S~N:3 as mpstthree::binary::Session>::Dual,
+                            )(
+                                S~N:3, // S(diff * (diff + 1) / 2 + K + i + j) (with Dual if needed)
                             )0*
                             ~( // j in  0..3
                                 R~N:4, // R(3 * (i - 1) + 1 + j)
                                 // Side note: we lose the checking for the right order for the name on R(3 * (i - 1) + 3) → RoleADual<RoleEnd>
-                            )()2*
+                            )()3*
                         >,
                     )0:0
 
-
                     $role<
-                        R^N:0, // R(3K-2)
-                        R^N:1, // R(3K-1)
+                        #(
+                            R#N:0,
+                        )13:0
                     >,
 
                     $name<mpstthree::role::end::RoleEnd>,
                 >,
             )
-            // -> $sessionmpst_name<
-            //     #( // i in K-1..0
-            //         S#N:0, // S(i) or  S(i + diff * (diff + 1))
-            //     )12:0
-            //     R#N:3, // R(3K-2) or R(3K-1)
-            //     $sender<mpstthree::role::end::RoleEnd>
-            // >
+            -> $sessionmpst_name<
+                #( // K-1 + i in (K-1..0)
+                    <S#N:8 as mpstthree::binary::Session>::Dual, // S(i) or  S(i + diff * (diff + 1))
+                )0:0
+                R^N:0, // R(3K-2) or R(3K-1)
+                $name<mpstthree::role::end::RoleEnd>
+            >
             where
                 #( // i in 1..(diff * (diff + 1) + 1)
                     S#N:0: mpstthree::binary::Session + 'a, // S(i)
@@ -78,64 +71,156 @@ macro_rules! create_choose_mpst_session_multi {
                     R#N:0: mpstthree::role::Role + 'a, // R(i)
                 )11:0
             {
+                #( // i in 1..(diff * (diff + 1))
+                    let (channel_#N:3, channel_#N:4) = S#N:0::new(); // channel_(get from matrix), channel_(opposite get from matrix) = S(i)
+                )4:0
 
+                #( // i in 1..K
+                    let (_, role_#N:0) = R#N:5::new();
+                )0:0
+                let (role_^N:2, _) = R^N:0::new();
 
-                // #( // i in 1..(diff * (diff + 1))
-                //     let (channel_#N:3, channel_#N:4) = S#N:0::new(); // channel_(get from matrix), channel_(opposite get from matrix) = S(i)
-                // )4:0
+                #( // i in 1..K
+                    let (name_#N:0, _) = <R#N:6 as mpstthree::role::Role>::Dual::new();
+                )0:0
+                let (name_^N:2, _) = $name::<mpstthree::role::end::RoleEnd>::new();
 
-                // #( // i in 1..K
-                //     let (_, role_#N:0) = R#N:5::new();
-                // )
-                // let (_, role_1) = R0::new();
-                // let (_, role_2) = R2::new();
-                // let (role_3, _) = R4::new();
+                #( // i in 1..K
+                    let choice_#N:0 = $sessionmpst_name {
+                            ~(
+                                session#N:1 : channel_~N:5,
+                            )(
+                                session#N:1 : channel_~N:5,
+                            )0*
+                            stack: role_#N:0,
+                            name: name_#N:0,
+                        };
+                )0:0
 
-                // let (name_1, _) =
-                //     <$dual_1<mpstthree::role::end::RoleEnd> as mpstthree::role::Role>::Dual::new();
-                // let (name_2, _) =
-                //     <$dual_2<mpstthree::role::end::RoleEnd> as mpstthree::role::Role>::Dual::new();
-                // let (name_3, _) = $sender::<mpstthree::role::end::RoleEnd>::new();
+                #( // i in 1..K
+                    let new_session_#N:0 = mpstthree::binary::send(either::Either::Left(choice_#N:0), s.session#N:0);
+                )0:0
+                let (_, new_queue) = $recv_func(s.stack);
 
-                // let choice_1 = $type_name {
-                //     session1: session_1_2,
-                //     session2: session_1_3,
-                //     stack: role_1,
-                //     name: name_1,
-                // };
+                let s = $sessionmpst_name {
+                    #(
+                        session#N:0: new_session_#N:0,
+                    )0:0
+                    stack: new_queue,
+                    name: s.name,
+                };
 
-                // let choice_2 = $type_name {
-                //     session1: session_2_1,
-                //     session2: session_2_3,
-                //     stack: role_2,
-                //     name: name_2,
-                // };
+                mpstthree::binary::cancel(s);
 
-                // let new_session_1 = mpstthree::binary::send(either::Either::Left(choice_1), s.session1);
-                // let new_session_2 = mpstthree::binary::send(either::Either::Left(choice_2), s.session2);
-                // let (_, new_queue) = $next(s.stack);
-
-                // let s = $type_name {
-                //     session1: new_session_1,
-                //     session2: new_session_2,
-                //     stack: new_queue,
-                //     name: s.name,
-                // };
-
-                // mpstthree::binary::cancel(s);
-
-                // $type_name {
-                //     session1: session_3_1,
-                //     session2: session_3_2,
-                //     stack: role_3,
-                //     name: name_3,
-                // }
-
-
+                $sessionmpst_name {
+                    #(
+                        session#N:0: channel_#N:7 ,
+                    )0:0
+                    stack: role_^N:2,
+                    name: name_^N:2,
+                }
             }
+        });
+    }
+}
+#[macro_export]
+macro_rules! create_choose_mpst_session_multi_right {
+    ($func_name:ident, $type_name: ident, $role:ident, $recv_func:ident, $name:ident, $sessionmpst_name:ident, $nsessions:literal, $exclusion:literal) => {
+        mpst_seq::seq!(N in 1..$nsessions ! $exclusion {
+            fn $func_name<'a, #(S#N:0,)10:0 #(R#N:0,)11:0>(
+                s: $sessionmpst_name<
+                    #( // i in 1..K
+                        $type_name<
+                            ~( // j in 0..K
+                                <S~N:2 as mpstthree::binary::Session>::Dual,
+                            )(
+                                S~N:2, // S(i + j) (with Dual if needed)
+                            )0*
+                            ~( // j in 0..K
+                                <S~N:3 as mpstthree::binary::Session>::Dual,
+                            )(
+                                S~N:3, // S(diff * (diff + 1) / 2 + K + i + j) (with Dual if needed)
+                            )0*
+                            ~( // j in  0..3
+                                R~N:4, // R(3 * (i - 1) + 1 + j)
+                                // Side note: we lose the checking for the right order for the name on R(3 * (i - 1) + 3) → RoleADual<RoleEnd>
+                            )()3*
+                        >,
+                    )0:0
 
+                    $role<
+                        #(
+                            R#N:0,
+                        )13:0
+                    >,
 
+                    $name<mpstthree::role::end::RoleEnd>,
+                >,
+            )
+            -> $sessionmpst_name<
+                #( // K-1 + i in (K-1..0)
+                    <S#N:9 as mpstthree::binary::Session>::Dual, // S(i) or  S(i + diff * (diff + 1))
+                )0:0
+                R^N:1, // R(3K-2) or R(3K-1)
+                $name<mpstthree::role::end::RoleEnd>
+            >
+            where
+                #( // i in 1..(diff * (diff + 1) + 1)
+                    S#N:0: mpstthree::binary::Session + 'a, // S(i)
+                )10:0
+                #( // i in 1..(3 * K)
+                    R#N:0: mpstthree::role::Role + 'a, // R(i)
+                )11:0
+            {
+                #( // i in 1..(diff * (diff + 1))
+                    let (channel_#N:3, channel_#N:4) = S#N:10::new(); // channel_(get from matrix), channel_(opposite get from matrix) = S(i)
+                )4:0
 
+                #( // i in 1..K
+                    let (_, role_#N:0) = R#N:11::new();
+                )0:0
+                let (role_^N:2, _) = R^N:1::new();
+
+                #( // i in 1..K
+                    let (name_#N:0, _) = <R#N:6 as mpstthree::role::Role>::Dual::new();
+                )0:0
+                let (name_^N:2, _) = $name::<mpstthree::role::end::RoleEnd>::new();
+
+                #( // i in 1..K
+                    let choice_#N:0 = $sessionmpst_name {
+                            ~(
+                                session#N:1 : channel_~N:5,
+                            )(
+                                session#N:1 : channel_~N:5,
+                            )0*
+                            stack: role_#N:0,
+                            name: name_#N:0,
+                        };
+                )0:0
+
+                #( // i in 1..K
+                    let new_session_#N:0 = mpstthree::binary::send(either::Either::Right(choice_#N:0), s.session#N:0);
+                )0:0
+                let (_, new_queue) = $recv_func(s.stack);
+
+                let s = $sessionmpst_name {
+                    #(
+                        session#N:0: new_session_#N:0,
+                    )0:0
+                    stack: new_queue,
+                    name: s.name,
+                };
+
+                mpstthree::binary::cancel(s);
+
+                $sessionmpst_name {
+                    #(
+                        session#N:0: channel_#N:7 ,
+                    )0:0
+                    stack: role_^N:2,
+                    name: name_^N:2,
+                }
+            }
         });
     }
 }
