758b9c819e14c0625bc2444cebdc5be18ed32f78
Lag <lagaillardie.nicolas@live.fr>
2020-11-19 10:21:21 +0100

    Fixed issue with beta

--- tests/sessionmpst.rs
+++ tests/sessionmpst.rs
@@ -24,7 +24,7 @@ fn sessionmpst_fields() {
     let (sessionmpst_1, sessionmpst_2) = Endpoint::<i32>::new();
 
     // sessionmpst_1
-    let (here1_stack, there1_stack) = RoleC::new();
+    let (here1_stack, there1_stack) = RoleC::<RoleEnd>::new();
     let (here1_name, there1_name) = RoleEnd::new();
     sessionmpst_1.stack.sender.send(there1_stack).unwrap_or(());
     sessionmpst_1.name.sender.send(there1_name).unwrap_or(());
@@ -33,7 +33,7 @@ fn sessionmpst_fields() {
     assert_eq!(here1_name.sender.send(()).unwrap_or(()), ());
 
     // sessionmpst_2
-    let (here2_stack, there2_stack) = RoleC::new();
+    let (here2_stack, there2_stack) = RoleC::<RoleEnd>::new();
     let (here2_name, there2_name) = RoleEnd::new();
     sessionmpst_2.stack.sender.send(here2_stack).unwrap_or(());
     sessionmpst_2.name.sender.send(here2_name).unwrap_or(());
