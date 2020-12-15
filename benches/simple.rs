1aa2058a635d8450c0ab44b8591404f7c47fa4cd
Lag <lagaillardie.nicolas@live.fr>
2020-12-15 21:47:45 +0100

    Started benchmarks

--- benches/simple.rs
+++ benches/simple.rs
@@ -1,18 +1,28 @@
-// use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-// fn fibonacci(n: u64) -> u64 {
-//     match n {
-//         0 => 1,
-//         1 => 1,
-//         n => fibonacci(n - 1) + fibonacci(n - 2),
-//     }
-// }
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
 
-// fn criterion_benchmark(c: &mut Criterion) {
-//     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-// }
+fn fibonacci2(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
 
-// criterion_group!(benches, criterion_benchmark);
-// criterion_main!(benches);
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+}
 
-fn main() {}
+fn criterion_benchmark2(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci2(black_box(20))));
+}
+
+criterion_group!(benches, criterion_benchmark, criterion_benchmark2);
+criterion_main!(benches);
