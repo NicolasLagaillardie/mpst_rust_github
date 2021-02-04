d33a71a96302a2122b29a14164522b8129c500bf
Lag <lagaillardie.nicolas@live.fr>
2021-02-04 19:11:54 +0100

    Updated script full_compile

--- scripts/full_compile.sh
+++ scripts/full_compile.sh
@@ -23,26 +23,26 @@ set -e
 # cargo flamegraph --example long_simple_eleven_binary -o flamegraphs/flamegraph_long_simple_eleven_binary.svg
 # cargo flamegraph --example long_simple_eleven_mpst -o flamegraphs/flamegraph_long_simple_eleven_mpst.svg
 
-./compile.sh long_simple_three_binary 
-./compile.sh long_simple_three_mpst
-./compile.sh long_simple_four_binary
-./compile.sh long_simple_four_mpst 
-./compile.sh long_simple_five_binary
-./compile.sh long_simple_five_mpst
-./compile.sh long_simple_six_binary
-./compile.sh long_simple_six_mpst 
-./compile.sh long_simple_seven_binary 
-./compile.sh long_simple_seven_mpst
-./compile.sh long_simple_eight_binary 
-./compile.sh long_simple_eight_mpst 
-./compile.sh long_simple_nine_binary 
-./compile.sh long_simple_nine_mpst 
-./compile.sh long_simple_ten_binary 
-./compile.sh long_simple_ten_mpst 
-./compile.sh long_simple_eleven_binary 
-./compile.sh long_simple_eleven_mpst 
-./compile.sh long_simple_twenty_binary 
-./compile.sh long_simple_twenty_mpst 
+./scripts/compile.sh long_simple_three_binary 
+# ./scripts/compile.sh long_simple_three_mpst
+# ./scripts/compile.sh long_simple_four_binary
+# ./scripts/compile.sh long_simple_four_mpst 
+# ./scripts/compile.sh long_simple_five_binary
+# ./scripts/compile.sh long_simple_five_mpst
+# ./scripts/compile.sh long_simple_six_binary
+# ./scripts/compile.sh long_simple_six_mpst 
+# ./scripts/compile.sh long_simple_seven_binary 
+# ./scripts/compile.sh long_simple_seven_mpst
+# ./scripts/compile.sh long_simple_eight_binary 
+# ./scripts/compile.sh long_simple_eight_mpst 
+# ./scripts/compile.sh long_simple_nine_binary 
+# ./scripts/compile.sh long_simple_nine_mpst 
+# ./scripts/compile.sh long_simple_ten_binary 
+# ./scripts/compile.sh long_simple_ten_mpst 
+# ./scripts/compile.sh long_simple_eleven_binary 
+# ./scripts/compile.sh long_simple_eleven_mpst 
+# ./scripts/compile.sh long_simple_twenty_binary 
+# ./scripts/compile.sh long_simple_twenty_mpst 
 
 # python create_graph_compile.py 
 
