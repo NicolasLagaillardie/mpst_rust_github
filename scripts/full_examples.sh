00e2d82f8f36f58c6315105734ec2ee924792080
Lag <lagaillardie.nicolas@live.fr>
2021-04-08 11:59:01 +0200

    fixed workflow examples

---
+++ scripts/full_examples.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# find ./examples/. -type f -exec sh -c 'for example in "$@"; do (cargo run --example ${example:13:-3}) done' argv0 {} +
+
+cargo run --example actyx_os_1
+cargo run --example actyx_os_2
+cargo run --example long_simple_three_mpst
+cargo run --example long_simple_four_mpst
+cargo run --example long_simple_five_mpst
+cargo run --example long_simple_six_mpst
+cargo run --example long_simple_seven_mpst
+cargo run --example long_simple_eight_mpst
+cargo run --example long_simple_nine_mpst
+cargo run --example long_simple_ten_mpst
+cargo run --example long_simple_eleven_mpst
+cargo run --example long_simple_twenty_mpst
