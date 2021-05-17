6f75a4d16095ae40393b737a16f4bc08071ee958
Lag <lagaillardie.nicolas@live.fr>
2021-05-17 17:46:14 +0200

    removed examples to be checked

--- scripts/full_examples.sh
+++ scripts/full_examples.sh
@@ -4,7 +4,7 @@ set -e
 
 # find ./examples/. -type f -exec sh -c 'for example in "$@"; do (cargo run --example ${example:13:-3}) done' argv0 {} +
 
-cargo run --example actyx_os_1
+# cargo run --example actyx_os_1
 # cargo run --example actyx_os_2
 # cargo run --example long_simple_three_mpst
 # cargo run --example long_simple_four_mpst
