7776394d5f3daa04b268d946eb1bd1caf56045fa
Lag <lagaillardie.nicolas@live.fr>
2021-05-20 18:38:44 +0200

    gitlab workflow

--- scripts/full_examples.sh
+++ scripts/full_examples.sh
@@ -6,13 +6,3 @@ set -e
 
 # cargo run --example actyx_os_1
 # cargo run --example actyx_os_2
-# cargo run --example long_simple_three_mpst
-# cargo run --example long_simple_four_mpst
-# cargo run --example long_simple_five_mpst
-# cargo run --example long_simple_six_mpst
-# cargo run --example long_simple_seven_mpst
-# cargo run --example long_simple_eight_mpst
-# cargo run --example long_simple_nine_mpst
-# cargo run --example long_simple_ten_mpst
-# cargo run --example long_simple_eleven_mpst
-# cargo run --example long_simple_twenty_mpst
