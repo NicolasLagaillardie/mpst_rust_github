3c71ae125fd63aa5ce3ad896dbc56bc7025f92fc
Lag <lagaillardie.nicolas@live.fr>
2021-06-29 17:20:24 +0200

    added dns

--- examples/dns.rs
+++ examples/dns.rs
@@ -1,14 +1,14 @@
-use mpstthree::binary::struct_trait::{End, Recv, Send, Session};
+use mpstthree::binary::struct_trait::{End, Recv, Send};
 use mpstthree::role::end::RoleEnd;
 use mpstthree::{
-    bundle_struct_fork_close_multi, choose_mpst_multi_to_all, create_multiple_normal_role_short,
-    create_recv_mpst_session_bundle, create_send_mpst_session_bundle, offer_mpst,
+    bundle_struct_fork_close_multi, choose_mpst_create_multi_to_all,
+    create_multiple_normal_role_short, create_recv_mpst_session_bundle,
+    create_send_mpst_session_bundle, offer_mpst,
 };
 
 use mpstthree::role::broadcast::RoleBroadcast;
-use rand::{thread_rng, Rng};
+use rand::{random, thread_rng, Rng};
 use std::error::Error;
-use std::marker;
 
 // explicit global protocol Dns(role Client, role Server, role Other)
 // {
@@ -29,256 +29,186 @@ bundle_struct_fork_close_multi!(close_mpst_multi, fork_mpst, SessionMpstThree, 3
 
 // Create new Roles
 // normal
-create_multiple_normal_role_short!(Voter, Pawn, Server);
+create_multiple_normal_role_short!(Client, Other, Server);
 
 // Create new send functions
+// CLIENT
+create_send_mpst_session_bundle!(
+    send_mpst_client_to_other, RoleOther, 1 |
+    send_mpst_client_to_server, RoleServer, 2 | =>
+    RoleClient, SessionMpstThree, 3
+);
+// OTHER
+create_send_mpst_session_bundle!(
+    send_mpst_other_to_client, RoleClient, 1 |
+    send_mpst_other_to_server, RoleServer, 2 | =>
+    RoleOther, SessionMpstThree, 3
+);
 // SERVER
 create_send_mpst_session_bundle!(
-    send_mpst_server_to_pawn, RolePawn, 1 |
-    send_mpst_server_to_voter, RoleVoter, 2 | =>
+    send_mpst_server_to_client, RoleClient, 1 |
+    send_mpst_server_to_other, RoleOther, 2 | =>
     RoleServer, SessionMpstThree, 3
-);
-// VOTER
-create_send_mpst_session_bundle!(
-    send_mpst_voter_to_pawn, RolePawn, 1 |
-    send_mpst_voter_to_server, RoleServer, 2 | =>
-    RoleVoter, SessionMpstThree, 3
 );
 
 // Create new recv functions and related types
-// PAWN
+// CLIENT
 create_recv_mpst_session_bundle!(
-    recv_mpst_pawn_to_server, RoleServer, 1 |
-    recv_mpst_pawn_to_voter, RoleVoter, 2 | =>
-    RolePawn, SessionMpstThree, 3
+    recv_mpst_client_from_other, RoleOther, 1 |
+    recv_mpst_client_from_server, RoleServer, 2 | =>
+    RoleClient, SessionMpstThree, 3
+);
+// OTHER
+create_recv_mpst_session_bundle!(
+    recv_mpst_other_from_client, RoleClient, 1 |
+    recv_mpst_other_from_server, RoleServer, 2 | =>
+    RoleOther, SessionMpstThree, 3
 );
 // SERVER
 create_recv_mpst_session_bundle!(
-    recv_mpst_server_to_pawn, RolePawn, 1 |
-    recv_mpst_server_to_voter, RoleVoter, 2 | =>
+    recv_mpst_server_from_client, RoleClient, 1 |
+    recv_mpst_server_from_other, RoleOther, 2 | =>
     RoleServer, SessionMpstThree, 3
-);
-// VOTER
-create_recv_mpst_session_bundle!(
-    recv_mpst_voter_to_pawn, RolePawn, 1 |
-    recv_mpst_voter_to_server, RoleServer, 2 | =>
-    RoleVoter, SessionMpstThree, 3
 );
 
 // Names
-type NamePawn = RolePawn<RoleEnd>;
+type NameClient = RoleClient<RoleEnd>;
+type NameOther = RoleOther<RoleEnd>;
 type NameServer = RoleServer<RoleEnd>;
-type NameVoter = RoleVoter<RoleEnd>;
 
 // Types
 // SERVER
-type Choose0fromStoP = Send<Branching0fromStoP, End>;
-type Choose0fromStoV<N> = Send<Branching0fromStoV<N>, End>;
-// VOTER
-type Choose1fromVtoP = <Choice1fromPtoV as Session>::Dual;
-type Choose1fromVtoS<N> = <Choice1fromStoV<N> as Session>::Dual;
-
-// VOTER
-enum Branching0fromStoV<N: marker::Send> {
-    Auth(
+type Choose0fromServerToClient = Send<Branching0fromServerToClient, End>;
+type Choose0fromServerToOther = Send<Branching0fromServerToOther, End>;
+// CLIENT
+enum Branching0fromServerToClient {
+    Dummy(SessionMpstThree<End, Recv<(i32, i32), End>, RoleServer<RoleEnd>, NameClient>),
+    Query(SessionMpstThree<End, Recv<(i32, i32), End>, RoleServer<RoleEnd>, NameClient>),
+}
+// OTHER
+enum Branching0fromServerToOther {
+    Dummy(SessionMpstThree<End, Recv<(), End>, RoleServer<RoleEnd>, NameOther>),
+    Query(
         SessionMpstThree<
-            Choose1fromVtoP,
-            Recv<N, Choose1fromVtoS<N>>,
-            RoleServer<RoleBroadcast>,
-            NameVoter,
+            End,
+            Recv<(i32, i32), Send<(i32, i32), End>>,
+            RoleServer<RoleServer<RoleEnd>>,
+            NameOther,
         >,
     ),
-    Reject(SessionMpstThree<End, Recv<N, End>, RoleServer<RoleEnd>, NameVoter>),
 }
-// PAWN
-enum Branching0fromStoP {
-    Auth(SessionMpstThree<End, Choice1fromPtoV, RoleVoter<RoleEnd>, NamePawn>),
-    Reject(SessionMpstThree<End, End, RoleEnd, NamePawn>),
-}
-enum Branching1fromVtoP {
-    Yes(SessionMpstThree<End, End, RoleEnd, NamePawn>),
-    No(SessionMpstThree<End, End, RoleEnd, NamePawn>),
-}
-type Choice1fromPtoV = Recv<Branching1fromVtoP, End>;
-// SERVER
-enum Branching1fromVtoS<N: marker::Send> {
-    Yes(SessionMpstThree<End, Recv<N, End>, RoleVoter<RoleEnd>, NameServer>),
-    No(SessionMpstThree<End, Recv<N, End>, RoleVoter<RoleEnd>, NameServer>),
-}
-type Choice1fromStoV<N> = Recv<Branching1fromVtoS<N>, End>;
 
 // Creating the MP sessions
-// VOTER
-type ChoiceVoter<N> = SessionMpstThree<
-    Choose1fromVtoP,
-    Recv<N, Choose1fromVtoS<N>>,
-    RoleServer<RoleBroadcast>,
-    NameVoter,
->;
-type EndpointVoter<N> = SessionMpstThree<
+// CLIENT
+type EndpointClient = SessionMpstThree<
     End,
-    Send<N, Recv<Branching0fromStoV<N>, End>>,
+    Send<i32, Recv<Branching0fromServerToClient, End>>,
     RoleServer<RoleServer<RoleEnd>>,
-    NameVoter,
+    NameClient,
 >;
-// PAWN
-type ChoicePawn = SessionMpstThree<End, Choice1fromPtoV, RoleVoter<RoleEnd>, NamePawn>;
-type EndpointPawn =
-    SessionMpstThree<Recv<Branching0fromStoP, End>, End, RoleServer<RoleEnd>, NamePawn>;
+// OTHER
+type EndpointOther =
+    SessionMpstThree<End, Recv<Branching0fromServerToOther, End>, RoleServer<RoleEnd>, NameOther>;
 // SERVER
-type ChoiceServer<N> = SessionMpstThree<End, Choice1fromStoV<N>, RoleVoter<RoleEnd>, NameServer>;
-type EndpointServer<N> = SessionMpstThree<
-    Choose0fromStoP,
-    Recv<N, Choose0fromStoV<N>>,
-    RoleVoter<RoleBroadcast>,
+type EndpointServer = SessionMpstThree<
+    Recv<i32, Choose0fromServerToClient>,
+    Choose0fromServerToOther,
+    RoleClient<RoleBroadcast>,
     NameServer,
 >;
 
+choose_mpst_create_multi_to_all!(
+    choose_mpst_server_to_all,
+    RoleClient,
+    RoleOther, =>
+    RoleServer,
+    SessionMpstThree,
+    3
+);
+
 // Functions
-fn endpoint_voter(s: EndpointVoter<i32>) -> Result<(), Box<dyn Error>> {
-    let auth = thread_rng().gen_range(1..=3);
+fn endpoint_client(s: EndpointClient) -> Result<(), Box<dyn Error>> {
+    let address = random::<i32>();
 
-    let s = send_mpst_voter_to_server(auth, s);
+    let s = send_mpst_client_to_server(address, s);
 
-    offer_mpst!(s, recv_mpst_voter_to_server, {
-        Branching0fromStoV::Reject(s) => {
+    offer_mpst!(s, recv_mpst_client_from_server, {
+        Branching0fromServerToClient::Dummy(s) => {
 
-            let (_, s) = recv_mpst_voter_to_server(s)?;
+            let (_, s) = recv_mpst_client_from_server(s)?;
 
             close_mpst_multi(s)
         },
-        Branching0fromStoV::Auth(s) => {
-            choice_voter(s)
+        Branching0fromServerToClient::Query(s) => {
+
+            let (_, s) = recv_mpst_client_from_server(s)?;
+
+            close_mpst_multi(s)
         },
     })
 }
 
-fn choice_voter(s: ChoiceVoter<i32>) -> Result<(), Box<dyn Error>> {
-    let (ok, s) = recv_mpst_voter_to_server(s)?;
+fn endpoint_other(s: EndpointOther) -> Result<(), Box<dyn Error>> {
+    offer_mpst!(s, recv_mpst_other_from_server, {
+        Branching0fromServerToOther::Dummy(s) => {
+            let (_, s) = recv_mpst_other_from_server(s)?;
+            close_mpst_multi(s)
+        },
+        Branching0fromServerToOther::Query(s) => {
+            let ((address, packet), s) = recv_mpst_other_from_server(s)?;
 
-    let expected = thread_rng().gen_range(1..=3);
+            let s = send_mpst_other_to_server((-address, -packet), s);
 
-    if ok == expected {
-        let s = choose_mpst_multi_to_all!(
+            close_mpst_multi(s)
+        },
+    })
+}
+
+fn endpoint_server(s: EndpointServer) -> Result<(), Box<dyn Error>> {
+    let choice = thread_rng().gen_range(1..3);
+
+    let (address, s) = recv_mpst_server_from_client(s)?;
+
+    if choice == 1 {
+        let s = choose_mpst_server_to_all!(
             s,
-            Branching1fromVtoP::Yes,
-            Branching1fromVtoS::<i32>::Yes, =>
-            RolePawn,
-            RoleServer, =>
-            RoleVoter,
-            SessionMpstThree,
-            3
+            Branching0fromServerToClient::Dummy,
+            Branching0fromServerToOther::Dummy
         );
 
-        let s = send_mpst_voter_to_server(1, s);
+        let s = send_mpst_server_to_other((), s);
+
+        let packet = random::<i32>();
+
+        let s = send_mpst_server_to_client((address, packet), s);
 
         close_mpst_multi(s)
     } else {
-        let s = choose_mpst_multi_to_all!(
+        let s = choose_mpst_server_to_all!(
             s,
-            Branching1fromVtoP::No,
-            Branching1fromVtoS::<i32>::No, =>
-            RolePawn,
-            RoleServer, =>
-            RoleVoter,
-            SessionMpstThree,
-            3
+            Branching0fromServerToClient::Query,
+            Branching0fromServerToOther::Query
         );
 
-        let s = send_mpst_voter_to_server(0, s);
+        let packet = random::<i32>();
+
+        let s = send_mpst_server_to_other((address, packet), s);
+
+        let ((new_address, new_packet), s) = recv_mpst_server_from_other(s)?;
+
+        let s = send_mpst_server_to_client((new_address, new_packet), s);
 
         close_mpst_multi(s)
     }
-}
-
-fn endpoint_pawn(s: EndpointPawn) -> Result<(), Box<dyn Error>> {
-    offer_mpst!(s, recv_mpst_pawn_to_server, {
-        Branching0fromStoP::Reject(s) => {
-            close_mpst_multi(s)
-        },
-        Branching0fromStoP::Auth(s) => {
-            choice_pawn(s)
-        },
-    })
-}
-
-fn choice_pawn(s: ChoicePawn) -> Result<(), Box<dyn Error>> {
-    offer_mpst!(s, recv_mpst_pawn_to_voter, {
-        Branching1fromVtoP::Yes(s) => {
-            close_mpst_multi(s)
-        },
-        Branching1fromVtoP::No(s) => {
-            close_mpst_multi(s)
-        },
-    })
-}
-
-fn endpoint_server(s: EndpointServer<i32>) -> Result<(), Box<dyn Error>> {
-    let choice = thread_rng().gen_range(1..=3);
-
-    let (auth, s) = recv_mpst_server_to_voter(s)?;
-
-    if choice == auth {
-        let s = choose_mpst_multi_to_all!(
-            s,
-            Branching0fromStoP::Reject,
-            Branching0fromStoV::<i32>::Reject, =>
-            RolePawn,
-            RoleVoter, =>
-            RoleServer,
-            SessionMpstThree,
-            2
-        );
-
-        let s = send_mpst_server_to_voter(0, s);
-
-        close_mpst_multi(s)
-    } else {
-        let s = choose_mpst_multi_to_all!(
-            s,
-            Branching0fromStoP::Auth,
-            Branching0fromStoV::<i32>::Auth, =>
-            RolePawn,
-            RoleVoter, =>
-            RoleServer,
-            SessionMpstThree,
-            2
-        );
-
-        let s = send_mpst_server_to_voter(1, s);
-
-        choice_server(s)
-    }
-}
-
-fn choice_server(s: ChoiceServer<i32>) -> Result<(), Box<dyn Error>> {
-    offer_mpst!(s, recv_mpst_server_to_voter, {
-        Branching1fromVtoS::<i32>::Yes(s) => {
-
-            let (answer, s) = recv_mpst_server_to_voter(s)?;
-
-            assert_eq!(answer, 1);
-
-            close_mpst_multi(s)
-        },
-        Branching1fromVtoS::<i32>::No(s) => {
-
-            let (answer, s) = recv_mpst_server_to_voter(s)?;
-
-            assert_eq!(answer, 0);
-
-            close_mpst_multi(s)
-        },
-    })
 }
 
 fn all_mpst() -> Result<(), Box<dyn std::any::Any + std::marker::Send>> {
-    let (thread_pawn, thread_server, thread_voter) =
-        fork_mpst(endpoint_pawn, endpoint_server, endpoint_voter);
+    let (thread_other, thread_server, thread_client) =
+        fork_mpst(endpoint_client, endpoint_other, endpoint_server);
 
-    thread_voter.join()?;
+    thread_client.join()?;
     thread_server.join()?;
-    thread_pawn.join()?;
+    thread_other.join()?;
 
     Ok(())
 }
