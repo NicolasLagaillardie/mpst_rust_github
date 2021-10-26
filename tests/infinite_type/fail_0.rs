f6355786bccfcbe494925a9ccfa915e6c7ce95a3
Nico <lagaillardie.nicolas@live.fr>
2021-10-26 16:14:04 +0100

    fixed tests

--- tests/infinite_type/fail_0.rs
+++ tests/infinite_type/fail_0.rs
@@ -1,27 +1,8 @@
-use mpstthree::binary::cancel::cancel;
-use mpstthree::binary::choose::*;
-use mpstthree::binary::close::close;
-use mpstthree::binary::fork::fork;
-use mpstthree::binary::fork::fork_with_thread_id;
-use mpstthree::binary::offer::*;
-use mpstthree::binary::recv::recv;
-use mpstthree::binary::select::select_mut;
-use mpstthree::binary::send::send;
 use mpstthree::binary::struct_trait::end::*;
 use mpstthree::binary::struct_trait::recv::*;
 use mpstthree::binary::struct_trait::send::*;
-use mpstthree::binary::struct_trait::session::*;
-use mpstthree::choose;
-use mpstthree::offer;
 
-use rand::{thread_rng, Rng};
-use std::boxed::Box;
-use std::error::Error;
 use std::marker;
-use std::mem;
-use std::sync::mpsc;
-use std::thread::sleep;
-use std::time::Duration;
 
 // Bug with the constraint checker.
 #[allow(dead_code)]
@@ -33,3 +14,5 @@ enum CalcOp2<N: marker::Send> {
 
 #[allow(dead_code)]
 type NiceCalcServer2<N> = Recv<CalcOp2<N>, End>;
+
+fn main() {}
