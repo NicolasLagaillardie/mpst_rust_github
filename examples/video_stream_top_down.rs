3845d5f59b4bb430d3b41860546913c92c8fe431
Lag <lagaillardie.nicolas@live.fr>
2021-12-21 10:54:03 +0100

    changed compiled examples

--- examples/video_stream_top_down.rs
+++ examples/video_stream_top_down.rs
@@ -1,177 +1,107 @@
-#![allow(clippy::type_complexity)]
-
-use rand::{thread_rng, Rng};
-
-use mpstthree::binary::struct_trait::{end::End, recv::Recv, send::Send, session::Session};
-use mpstthree::functionmpst::close::close_mpst;
-use mpstthree::functionmpst::fork::fork_mpst;
-use mpstthree::meshedchannels::MeshedChannels;
+use mpstthree::binary::struct_trait::{end::End, recv::Recv, send::Send};
+use mpstthree::bundle_impl_with_enum_and_cancel;
 use mpstthree::role::broadcast::RoleBroadcast;
-
-use std::boxed::Box;
-use std::error::Error;
-use std::marker;
-
-// Get roles
-use mpstthree::role::a::RoleA;
-use mpstthree::role::b::RoleB;
-use mpstthree::role::c::RoleC;
 use mpstthree::role::end::RoleEnd;
+use std::error::Error;
+bundle_impl_with_enum_and_cancel!(MeshedChannels, AUTH, CLIENT, SERVER,);
 
-// Get recv functions
-use mpstthree::functionmpst::recv::recv_mpst_a_from_b;
-use mpstthree::functionmpst::recv::recv_mpst_a_from_c;
-use mpstthree::functionmpst::recv::recv_mpst_b_from_a;
-use mpstthree::functionmpst::recv::recv_mpst_c_from_a;
+type RequestVideoSERVERtoAUTH = Recv<i32, Send<i32, End>>;
 
-// Get send functions
-use mpstthree::functionmpst::send::send_mpst_a_to_b;
-use mpstthree::functionmpst::send::send_mpst_a_to_c;
-use mpstthree::functionmpst::send::send_mpst_b_to_a;
-use mpstthree::functionmpst::send::send_mpst_c_to_a;
+type OrderingServer10 = RoleClient<RoleEnd>;
+type OrderingServer11Full = RoleAuth<RoleAuth<OrderingServer10>>;
+type EndpointServer12 = MeshedChannels<
+    RequestVideoSERVERtoAUTH,
+    Recv<Branches0ServertoClient, End>,
+    OrderingServer11Full,
+    RoleServer<RoleEnd>,
+>;
 
-use mpstthree::choose_mpst_c_to_all;
-use mpstthree::offer_mpst_a_to_c;
-use mpstthree::offer_mpst_b_to_c;
+type CloseSERVERtoAUTH = Recv<(), End>;
 
-// See the folder scribble_protocols for the related Scribble protocol
+type OrderingServer13Full = RoleAuth<RoleEnd>;
+type EndpointServer14 =
+    MeshedChannels<CloseSERVERtoAUTH, End, OrderingServer13Full, RoleServer<RoleEnd>>;
 
-// Test our usecase Video Stream
-// Simple types
-// Client = C
-// Authenticator = A
-// Server = B
-
-type AtoCClose = End;
-type AtoBClose = End;
-type AtoBVideo<N> = Send<N, Recv<N, End>>;
-type AtoCVideo<N> = Recv<N, Send<N, RecursAtoC<N>>>;
-
-type InitA<N> = Recv<N, Send<N, RecursAtoC<N>>>;
-
-type BtoAClose = <AtoBClose as Session>::Dual;
-type BtoCClose = End;
-type BtoAVideo<N> = <AtoBVideo<N> as Session>::Dual;
-
-type RecursAtoC<N> = Recv<Branches0AtoC<N>, End>;
-type RecursBtoC<N> = Recv<Branches0BtoC<N>, End>;
-
-enum Branches0AtoC<N: marker::Send> {
-    End(MeshedChannels<AtoBClose, AtoCClose, StackAEnd, RoleA<RoleEnd>>),
-    Video(MeshedChannels<AtoBVideo<N>, AtoCVideo<N>, StackAVideo, RoleA<RoleEnd>>),
+enum Branches0ServertoClient {
+    RequestVideo(EndpointServer12),
+    Close(EndpointServer14),
 }
-enum Branches0BtoC<N: marker::Send> {
-    End(MeshedChannels<BtoAClose, BtoCClose, StackBEnd, RoleB<RoleEnd>>),
-    Video(MeshedChannels<BtoAVideo<N>, RecursBtoC<N>, StackBVideo, RoleB<RoleEnd>>),
+type Choose0forServertoClient = Send<Branches0ServertoClient, End>;
+
+type OrderingServer35 = RoleClient<RoleEnd>;
+type OrderingServer36Full = OrderingServer35;
+type EndpointServer37 = MeshedChannels<
+    End,
+    Recv<Branches0ServertoClient, End>,
+    OrderingServer36Full,
+    RoleServer<RoleEnd>,
+>;
+
+type RequestVideoAUTHtoCLIENT = Recv<i32, Send<i32, Recv<Branches0AuthtoClient, End>>>;
+type RequestVideoAUTHtoSERVER = Send<i32, Recv<i32, End>>;
+
+type OrderingAuth10 = RoleClient<RoleEnd>;
+type OrderingAuth11Full = RoleClient<RoleServer<RoleServer<RoleClient<OrderingAuth10>>>>;
+type EndpointAuth12 = MeshedChannels<
+    RequestVideoAUTHtoCLIENT,
+    RequestVideoAUTHtoSERVER,
+    OrderingAuth11Full,
+    RoleAuth<RoleEnd>,
+>;
+
+type CloseAUTHtoCLIENT = Recv<(), End>;
+type CloseAUTHtoSERVER = Send<(), End>;
+
+type OrderingAuth13Full = RoleClient<RoleServer<RoleEnd>>;
+type EndpointAuth14 =
+    MeshedChannels<CloseAUTHtoCLIENT, CloseAUTHtoSERVER, OrderingAuth13Full, RoleAuth<RoleEnd>>;
+
+enum Branches0AuthtoClient {
+    RequestVideo(EndpointAuth12),
+    Close(EndpointAuth14),
 }
-type Choose0fromCtoA<N> = Send<Branches0AtoC<N>, End>;
-type Choose0fromCtoB<N> = Send<Branches0BtoC<N>, End>;
+type Choose0forAuthtoClient = Send<Branches0AuthtoClient, End>;
 
-type InitC<N> = Send<N, Recv<N, Choose0fromCtoA<N>>>;
+type DeclareAUTHtoCLIENT = Recv<i32, Send<i32, Recv<Branches0AuthtoClient, End>>>;
 
-// Stacks
-type StackAEnd = RoleEnd;
-type StackAVideo = RoleC<RoleB<RoleB<RoleC<RoleC<RoleEnd>>>>>;
-type StackARecurs = RoleC<RoleEnd>;
-type StackAInit = RoleC<RoleC<RoleC<RoleEnd>>>;
+type OrderingAuth35 = RoleClient<RoleEnd>;
+type OrderingAuth36Full = RoleClient<RoleClient<OrderingAuth35>>;
+type EndpointAuth37 =
+    MeshedChannels<DeclareAUTHtoCLIENT, End, OrderingAuth36Full, RoleAuth<RoleEnd>>;
 
-type StackBEnd = RoleEnd;
-type StackBVideo = RoleA<RoleA<RoleC<RoleEnd>>>;
-type StackBRecurs = RoleC<RoleEnd>;
+type DeclareCLIENTtoAUTH = Send<i32, Recv<i32, Choose0forAuthtoClient>>;
 
-type StackCRecurs = RoleBroadcast;
-type StackCFull = RoleA<RoleA<StackCRecurs>>;
+type CloseCLIENTtoAUTH = Send<(), End>;
 
-// Creating the MP sessions
+type OrderingClient6Full = RoleAuth<RoleBroadcast>;
+type EndpointClient7 = MeshedChannels<
+    CloseCLIENTtoAUTH,
+    Choose0forServertoClient,
+    OrderingClient6Full,
+    RoleClient<RoleEnd>,
+>;
 
-// For C
-type EndpointCRecurs<N> =
-    MeshedChannels<Choose0fromCtoA<N>, Choose0fromCtoB<N>, StackCRecurs, RoleC<RoleEnd>>;
-type EndpointCFull<N> = MeshedChannels<InitC<N>, Choose0fromCtoB<N>, StackCFull, RoleC<RoleEnd>>;
+type RequestVideoCLIENTtoAUTH = Send<i32, End>;
 
-// For A
-type EndpointARecurs<N> = MeshedChannels<End, RecursAtoC<N>, StackARecurs, RoleA<RoleEnd>>;
-type EndpointAFull<N> = MeshedChannels<End, InitA<N>, StackAInit, RoleA<RoleEnd>>;
+type OrderingClient4Full = RoleAuth<RoleBroadcast>;
+type EndpointClient5 = MeshedChannels<
+    RequestVideoCLIENTtoAUTH,
+    Choose0forServertoClient,
+    OrderingClient4Full,
+    RoleClient<RoleEnd>,
+>;
 
-// For B
-type EndpointBRecurs<N> = MeshedChannels<End, RecursBtoC<N>, StackBRecurs, RoleB<RoleEnd>>;
+type EndpointClient8 = MeshedChannels<
+    Choose0forAuthtoClient,
+    Choose0forServertoClient,
+    RoleBroadcast,
+    RoleClient<RoleEnd>,
+>;
 
-// Functions related to endpoints
-fn server(s: EndpointBRecurs<i32>) -> Result<(), Box<dyn Error>> {
-    offer_mpst_b_to_c!(s, {
-        Branches0BtoC::End(s) => {
-            close_mpst(s)
-        },
-        Branches0BtoC::Video(s) => {
-            let (request, s) = recv_mpst_b_from_a(s)?;
-            let s = send_mpst_b_to_a(request + 1, s);
-            server(s)
-        },
-    })
-}
-
-fn authenticator(s: EndpointAFull<i32>) -> Result<(), Box<dyn Error>> {
-    let (id, s) = recv_mpst_a_from_c(s)?;
-    let s = send_mpst_a_to_c(id + 1, s);
-
-    authenticator_recurs(s)
-}
-
-fn authenticator_recurs(s: EndpointARecurs<i32>) -> Result<(), Box<dyn Error>> {
-    offer_mpst_a_to_c!(s, {
-        Branches0AtoC::End(s) => {
-            close_mpst(s)
-        },
-        Branches0AtoC::Video(s) => {
-            let (request, s) = recv_mpst_a_from_c(s)?;
-            let s = send_mpst_a_to_b(request + 1, s);
-            let (video, s) = recv_mpst_a_from_b(s)?;
-            let s = send_mpst_a_to_c(video + 1, s);
-            authenticator_recurs(s)
-        },
-    })
-}
-
-fn client(s: EndpointCFull<i32>) -> Result<(), Box<dyn Error>> {
-    let mut rng = thread_rng();
-    let xs: Vec<i32> = (1..100).map(|_| rng.gen()).collect();
-
-    let s = send_mpst_c_to_a(0, s);
-    let (_, s) = recv_mpst_c_from_a(s)?;
-
-    client_recurs(s, xs, 1)
-}
-
-fn client_recurs(
-    s: EndpointCRecurs<i32>,
-    mut xs: Vec<i32>,
-    index: i32,
-) -> Result<(), Box<dyn Error>> {
-    match xs.pop() {
-        Option::Some(_) => {
-            let s = choose_mpst_c_to_all!(s, Branches0AtoC::Video, Branches0BtoC::Video);
-
-            let s = send_mpst_c_to_a(1, s);
-            let (_, s) = recv_mpst_c_from_a(s)?;
-
-            client_recurs(s, xs, index + 1)
-        }
-        Option::None => {
-            let s = choose_mpst_c_to_all!(s, Branches0AtoC::End, Branches0BtoC::End);
-
-            assert_eq!(index, 100);
-
-            close_mpst(s)
-        }
-    }
-}
-
-/////////////////////////////////////////
-
-fn main() {
-    let (thread_a, thread_s, thread_c) = fork_mpst(authenticator, server, client);
-
-    assert!(thread_a.join().is_ok());
-    assert!(thread_s.join().is_ok());
-    assert!(thread_c.join().is_ok());
-}
+type OrderingClient10Full = RoleAuth<RoleAuth<RoleEnd>>;
+type EndpointClient11 = MeshedChannels<
+    DeclareCLIENTtoAUTH,
+    Choose0forServertoClient,
+    OrderingClient10Full,
+    RoleClient<RoleEnd>,
+>;
--- scripts/ping_pong_mesh_ring_light.sh
+++ scripts/ping_pong_mesh_ring_light.sh
@@ -11,43 +11,43 @@ set -e
 ### Two
 ./scripts/create_files/compile.sh mesh_two_binary 10 baking
 ./scripts/create_files/compile.sh mesh_two_crossbeam 10 baking
-./scripts/create_files/compile.sh mesh_two_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh mesh_two_baking 10 baking
 
 ### Three
 ./scripts/create_files/compile.sh mesh_three_binary 10 baking
 ./scripts/create_files/compile.sh mesh_three_crossbeam 10 baking
-./scripts/create_files/compile.sh mesh_three_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh mesh_three_baking 10 baking
 
 ### Four
 ./scripts/create_files/compile.sh mesh_four_binary 10 baking
 ./scripts/create_files/compile.sh mesh_four_crossbeam 10 baking
-./scripts/create_files/compile.sh mesh_four_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh mesh_four_baking 10 baking
 
 ### Five
 ./scripts/create_files/compile.sh mesh_five_binary 10 baking
 ./scripts/create_files/compile.sh mesh_five_crossbeam 10 baking
-./scripts/create_files/compile.sh mesh_five_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh mesh_five_baking 10 baking
 
 ## Ring
 ### Two
 ./scripts/create_files/compile.sh ring_two_binary 10 baking
 ./scripts/create_files/compile.sh ring_two_crossbeam 10 baking
-./scripts/create_files/compile.sh ring_two_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh ring_two_baking 10 baking
 
 ### Three
 ./scripts/create_files/compile.sh ring_three_binary 10 baking
 ./scripts/create_files/compile.sh ring_three_crossbeam 10 baking
-./scripts/create_files/compile.sh ring_three_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh ring_three_baking 10 baking
 
 ### Four
 ./scripts/create_files/compile.sh ring_four_binary 10 baking
 ./scripts/create_files/compile.sh ring_four_crossbeam 10 baking
-./scripts/create_files/compile.sh ring_four_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh ring_four_baking 10 baking
 
 ### Five
 ./scripts/create_files/compile.sh ring_five_binary 10 baking
 ./scripts/create_files/compile.sh ring_five_crossbeam 10 baking
-./scripts/create_files/compile.sh ring_five_baking_cancel_inline 10 baking
+./scripts/create_files/compile.sh ring_five_baking 10 baking
 
 # Run the benchmarks
 cargo bench --bench ping_pong --features="baking"  -- --verbose
