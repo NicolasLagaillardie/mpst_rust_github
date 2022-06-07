504f893149d01906a9d607f9316edba5c8540e06
Lag <lagaillardie.nicolas@live.fr>
2022-06-07 14:59:42 +0200

    update scripts

--- scripts/publish_all.sh
+++ scripts/publish_all.sh
@@ -5,15 +5,21 @@
 # Exit if error
 set -e
 
-# mpst_seq
-cd mpst_seq/mpst_seq_proc/
+# mpst_seq_proc
+cd mpst_seq_proc/
+cargo publish --all-features --dry-run
 cargo publish --all-features
 sleep 5
 cd ..
+
+# mpst_seq
+cd mpst_seq/
+cargo publish --all-features --dry-run
 cargo publish --all-features
 sleep 5
 cd ..
 
 # final publish
+cargo publish --all-features --dry-run
 cargo publish --all-features
 echo "done"
