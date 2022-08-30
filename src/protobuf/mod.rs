f4e42d7927863418b073dd97847ea5f40e036e00
Lag <lagaillardie.nicolas@live.fr>
2022-08-30 13:55:42 +0200

    updated generated file

--- src/protobuf/mod.rs
+++ src/protobuf/mod.rs
@@ -1,11 +1,3 @@
-#![cfg(feature = "protobuf")]
-
-//! Module for top-down approach
-//! using nuscr and protobuf
+// @generated
 
 pub mod globaltype;
-
-/// Test
-pub fn test() -> String {
-    String::from("Test")
-}
