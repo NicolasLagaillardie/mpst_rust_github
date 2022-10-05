1144e1a7123b353c1c237c5b8f841049709b5626
Lag <lagaillardie.nicolas@live.fr>
2022-10-06 00:48:27 +0200

    updated selected examples scripts, still need rework, issue with some timed examples not run

--- scripts/create_graphs/examples_literature_affine_timed.py
+++ scripts/create_graphs/examples_literature_affine_timed.py
@@ -28,16 +28,16 @@ result_folder = Path('results/')
 json_path = '/base/estimates.json'
 
 # Expected compile files
-compile_files = ['three_buyers', 'distributed_calc', 'three_travel', 'simple_voting', 'online_wallet',
-                 'fib', 'o_auth', 'smtp']
+compile_files = ['three_buyers', 'distributed_calc', 'three_travel',
+                 'simple_voting', 'online_wallet', 'o_auth', 'smtp']
 
 # Expected bench files
-bench_files = ['Distributed calculator baking', 'Fibo MPST 20 baking', 'oAuth MPST baking', 'Online wallet baking',
-               'Simple voting MPST baking', 'SMTP baking', 'Travel MPST baking', 'Three buyers MPST baking']
+bench_files = ['Distributed calculator baking', 'oAuth MPST baking', 'Online wallet baking', 'Simple voting MPST baking', 'SMTP baking', 'Travel MPST baking', 'Three buyers MPST baking',
+               'Timed Distributed calculator baking', 'Timed oAuth MPST baking', 'Timed Online wallet baking', 'Timed Simple voting MPST baking', 'Timed SMTP baking', 'Timed Travel MPST baking', 'Timed Three buyers MPST baking']
 
 # Expected bench files
-translate = {'Distributed calculator baking': 'distributed_calc', 'Fibo MPST 20 baking': 'fib', 'oAuth MPST baking': 'o_auth', 'Online wallet baking': 'online_wallet',
-             'Simple voting MPST baking': 'simple_voting', 'SMTP baking': 'smtp', 'Travel MPST baking': 'three_travel', 'Three buyers MPST baking': 'three_buyers'}
+translate = {'Distributed calculator baking': 'distributed_calc', 'oAuth MPST baking': 'o_auth', 'Online wallet baking': 'online_wallet', 'Simple voting MPST baking': 'simple_voting', 'SMTP baking': 'smtp', 'Travel MPST baking': 'three_travel', 'Three buyers MPST baking': 'three_buyers', 'Timed Distributed calculator baking':
+             'Timed distributed_calc', 'Timed oAuth MPST baking': 'Timed o_auth', 'Timed Online wallet baking': 'Timed online_wallet', 'Timed Simple voting MPST baking': 'Timed simple_voting', 'Timed SMTP baking': 'Timed smtp', 'Timed Travel MPST baking': 'Timed three_travel', 'Timed Three buyers MPST baking': 'Timed three_buyers'}
 
 
 def test(path):
@@ -49,14 +49,12 @@ def test(path):
 
 # Lists for plots
 bench = {}
+compilation = {}
 
 # For each folder in criterion_path
 for d in criterion_directories:
-    # If name looks like the one from what we want
-    for bench_file in bench_files:
-        if bench_file in d:
-            bench[translate[bench_file]] = int(test(d))
-            break
+    if d in bench_files:
+        bench[translate[d]] = int(test(d))
 
 # Get index of new file
 index = 0
@@ -79,58 +77,7 @@ for d in compile_directories:
                             temp_check.append(int(line.split('; ')[1]))
                         elif 'build' in line:
                             temp_build.append(int(line.split('; ')[1]))
-                        elif 'release' in line:
-                            temp_release.append(int(line.split('; ')[1]))
 
-                result_file = 'benchmarks_main_from_literature_' + \
-                    str(index) + '.csv'
-
-                with open(result_folder / result_file, 'a') as report_file:
-
-                    try:
-                        report_file.write(compile_file)
-                        report_file.write('; ')
-                    except:
-                        print('Issue with compilation time of', d)
-                    try:
-                        report_file.write(str(statistics.mean(temp_check)))
-                        report_file.write('; ')
-                    except:
-                        print('Issue with checking time of', d)
-                    try:
-                        report_file.write(str(statistics.mean(temp_build)))
-                        report_file.write('; ')
-                    except:
-                        print('Issue with building time of', d)
-                    try:
-                        report_file.write(str(statistics.mean(temp_release)))
-                        report_file.write('; ')
-                    except:
-                        print('Issue with building --release time of', d)
-                    try:
-                        report_file.write(str(bench[compile_file]))
-                        report_file.write('; ')
-                    except:
-                        print('Issue with benchmarking time of', d)
-                    report_file.write('\n')
-
-                print(compile_file + " done")
-            except:
-                print('Issue with ', d)
-            break
-
-        elif compile_file in d and 'timed' in d:
-            try:
-                with open(compile_folder / d, 'r') as f:
-                    lines = [line.rstrip() for line in f]
-                    temp_check = []
-                    temp_build = []
-                    temp_release = []
-                    for line in lines:
-                        if 'check' in line:
-                            temp_check.append(int(line.split('; ')[1]))
-                        elif 'build' in line:
-                            temp_build.append(int(line.split('; ')[1]))
                         elif 'release' in line:
                             temp_release.append(int(line.split('; ')[1]))
 
@@ -172,3 +119,34 @@ for d in compile_directories:
             break
 
 print("Done")
+
+print("Results: ", bench, bench.__len__())
+
+# running_time_fig = plt.figure()
+
+# running_time = running_time_fig.add_subplot(1, 1, 1)
+# running_time.set_title("Running time", pad=10, fontsize=30)
+# running_time.xaxis.set_major_locator(MaxNLocator(integer=True))
+# running_time.yaxis.set_major_locator(MaxNLocator(integer=True))
+# running_time.set_ylabel('Time (ms)', fontsize=30)
+
+# running_time.hist()
+
+
+# compilation_time_fig = plt.figure()
+
+# compilation_time = compilation_time_fig.add_subplot(1, 1, 1)
+# compilation_time.set_title("Running time", pad=10, fontsize=30)
+# compilation_time.xaxis.set_major_locator(MaxNLocator(integer=True))
+# compilation_time.yaxis.set_major_locator(MaxNLocator(integer=True))
+# compilation_time.set_ylabel('Time (ms)', fontsize=30)
+
+
+# # adjust subplot position
+# plt.tight_layout()
+
+# # save the figure
+# # plt.savefig(name_graph)
+
+# # show the plot
+# plt.show()
--- scripts/examples_affine_timed_literature.sh
+++ scripts/examples_affine_timed_literature.sh
@@ -6,28 +6,52 @@
 set -e
 
 ## Compile basic examples
-bash ./scripts/create_files/compile_full.sh three_buyers 100 affine_timed
-bash ./scripts/create_files/compile_full.sh distributed_calc 100 affine_timed
-bash ./scripts/create_files/compile_full.sh three_travel 100 affine_timed
-bash ./scripts/create_files/compile_full.sh simple_voting 100 affine_timed
-bash ./scripts/create_files/compile_full.sh online_wallet 100 affine_timed
-bash ./scripts/create_files/compile_full.sh fib 100 affine_timed
-bash ./scripts/create_files/compile_full.sh o_auth 100 affine_timed
-bash ./scripts/create_files/compile_full.sh smtp 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh three_buyers 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh distributed_calc 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh three_travel 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh simple_voting 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh online_wallet 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh fib 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh o_auth 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh smtp 100 affine_timed
 
 ## Compile timed examples
-bash ./scripts/create_files/compile_full.sh three_buyers_timed 100 affine_timed
-bash ./scripts/create_files/compile_full.sh distributed_calc_timed 100 affine_timed
-bash ./scripts/create_files/compile_full.sh three_travel_timed 100 affine_timed
-bash ./scripts/create_files/compile_full.sh simple_voting_timed 100 affine_timed
-bash ./scripts/create_files/compile_full.sh online_wallet_timed 100 affine_timed
-bash ./scripts/create_files/compile_full.sh fib_timed 100 affine_timed
-bash ./scripts/create_files/compile_full.sh o_auth_timed 100 affine_timed
-bash ./scripts/create_files/compile_full.sh smtp_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh three_buyers_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh distributed_calc_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh three_travel_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh simple_voting_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh online_wallet_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh fib_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh o_auth_timed 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh smtp_timed 100 affine_timed
 
 ## Compile timed additional examples
-bash ./scripts/create_files/compile_full.sh remote_data 100 affine_timed
-bash ./scripts/create_files/compile_full.sh servo_8257_async_failing 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh remote_data 100 affine_timed
+bash ./scripts/create_files/compile_normal.sh servo_8257_async_failing 100 affine_timed
+
+# ## Compile basic examples
+# bash ./scripts/create_files/compile_full.sh three_buyers 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh distributed_calc 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh three_travel 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh simple_voting 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh online_wallet 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh fib 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh o_auth 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh smtp 100 affine_timed
+
+# ## Compile timed examples
+# bash ./scripts/create_files/compile_full.sh three_buyers_timed 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh distributed_calc_timed 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh three_travel_timed 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh simple_voting_timed 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh online_wallet_timed 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh fib_timed 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh o_auth_timed 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh smtp_timed 100 affine_timed
+
+# ## Compile timed additional examples
+# bash ./scripts/create_files/compile_full.sh remote_data 100 affine_timed
+# bash ./scripts/create_files/compile_full.sh servo_8257_async_failing 100 affine_timed
 
 ## Run benchmarks
 cargo clean
