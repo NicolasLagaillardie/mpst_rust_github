f6b44a0ec6a5780cf671858442a5de15f8890d51
Lag <lagaillardie.nicolas@live.fr>
2022-11-21 15:56:48 +0100

    added comments

--- mpst_seq_proc/src/session_timed/session.rs
+++ mpst_seq_proc/src/session_timed/session.rs
@@ -8,7 +8,7 @@ use syn::{
     GenericParam, Ident, Index, Item, ItemEnum, ItemStruct, ItemType, PathArguments, Result, Type,
 };
 
-// Get HashSet of params of the given type
+/// Get HashSet of params of the given type
 fn idents_set<P>(params: &Punctuated<GenericParam, P>) -> HashSet<Ident> {
     params
         .iter()
@@ -19,13 +19,13 @@ fn idents_set<P>(params: &Punctuated<GenericParam, P>) -> HashSet<Ident> {
         .collect::<HashSet<_>>()
 }
 
-// Extend `right` with `left`, then assign `right` value to `left
+/// Extend `right` with `left`, then assign `right` value to `left`
 fn punctuated_prepend<T, P: Default>(left: &mut Punctuated<T, P>, mut right: Punctuated<T, P>) {
     right.extend(mem::take(left));
     *left = right;
 }
 
-// Extract types from given type
+/// Extract types (parameters) from given type
 fn unroll_type(mut ty: &mut Type) -> &mut Type {
     loop {
         ty = match ty {
@@ -38,19 +38,23 @@ fn unroll_type(mut ty: &mut Type) -> &mut Type {
     ty
 }
 
-// Augment the provided type, and take the excluded types in consideration
+/// Augment/expand the provided type, and take the excluded types in consideration
 fn augment_type(mut ty: &mut Type, exclude: &HashSet<Ident>) {
     while let Type::Path(path) = unroll_type(ty) {
+        // If recursion on the same type, break
         if *path == parse_quote!(Self) {
             break;
         }
 
+        // If no element after the last element, break
         let segment = match path.path.segments.last_mut() {
             Some(segment) => segment,
             _ => break,
         };
 
+        // If end of arguments of segment and no loop, complete with angle brackets
         if let PathArguments::None = segment.arguments {
+            // If exclude does not contain ident of segment, break
             if exclude.contains(&segment.ident) {
                 break;
             }
@@ -58,18 +62,22 @@ fn augment_type(mut ty: &mut Type, exclude: &HashSet<Ident>) {
             segment.arguments = PathArguments::AngleBracketed(parse_quote!(<>));
         }
 
+        // Unroll and assign arguments of segment to args
         let args = match &mut segment.arguments {
             PathArguments::AngleBracketed(args) => &mut args.args,
             _ => break,
         };
 
+        // Complete args with parse_quote!('__r, __R)
         let is_empty = args.is_empty();
         punctuated_prepend(args, parse_quote!('__r, __R));
 
+        // If no arguments, break
         if is_empty {
             break;
         }
 
+        // Reassigne the arguments to the type ty
         ty = match args.last_mut() {
             Some(GenericArgument::Type(ty)) => ty,
             _ => break,
@@ -88,55 +96,79 @@ fn session_type(mut input: ItemType) -> TokenStream {
     input.into_token_stream()
 }
 
+/// If session_timed is an enum
 fn session_enum(mut input: ItemEnum) -> Result<TokenStream> {
+    // If no variants, return error
     if input.variants.is_empty() {
         let message = "expected at least one variant";
         return Err(Error::new_spanned(&input.variants, message));
     }
 
+    // Get Ident of type
     let ident = &input.ident;
+
+    // Get excluded idents set from generic parameters 
     let exclude = idents_set(&input.generics.params);
 
+    // Get genereics of input
     let mut generics = input.generics.clone();
+
+    // Concat params of generics with rumpsteak role and `__r
     punctuated_prepend(
         &mut generics.params,
         parse_quote!('__q, '__r, __R: ::rumpsteak::Role + '__r),
     );
+
+    // Split a type's generics into the pieces required for impl'ing a trait for that type 
     let (impl_generics, _, _) = generics.split_for_impl();
 
     let mut generics = input.generics.clone();
+
+    // Concat params of generics with rumpsteak role
     punctuated_prepend(
         &mut generics.params,
         parse_quote!('__q, __R: ::rumpsteak::Role),
     );
+
+    // Split a type's generics into the pieces required for impl'ing a trait for that type 
     let (_, ty_generics, where_clause) = generics.split_for_impl();
 
     let mut idents = Vec::with_capacity(input.variants.len());
     let mut labels = Vec::with_capacity(input.variants.len());
     let mut tys = Vec::with_capacity(input.variants.len());
 
+    // For each variant of the enum, augment the types it contains and add them to `tys`
     for variant in &mut input.variants {
         idents.push(&variant.ident);
+
+        // Extract all the fields of the variant
         let fields = match &mut variant.fields {
             Fields::Unnamed(fields) => Ok(&mut fields.unnamed),
             fields => Err(Error::new_spanned(fields, "expected tuple variants")),
         }?;
 
+        // If not exactly 2 fields
         if fields.len() != 2 {
             let message = "expected exactly two fields per variant";
             return Err(Error::new_spanned(fields, message));
         }
 
+        // Iter over fields, get encapsulated label and type
         let mut fields = fields.iter_mut();
 
         let label = &fields.next().unwrap().ty;
         labels.push(label);
 
         let ty = &mut fields.next().unwrap().ty;
+
+        // Augment encapsulated type
         augment_type(ty, &exclude);
+
+        // Add type ty to vec tys
         tys.push(&*ty);
     }
 
+    // For each label, add its related impl
     let mut output = TokenStream::new();
     for (label, ty) in labels.iter().zip(&tys) {
         output.extend(quote! {
@@ -146,26 +178,16 @@ fn session_enum(mut input: ItemEnum) -> Result<TokenStream> {
         });
     }
 
+    // Extend params of generics of input with rumpsteak role
     punctuated_prepend(
         &mut input.generics.params,
         parse_quote!('__r, __R: ::rumpsteak::Role),
     );
+
+    // Split a type's generics into the pieces required for impl'ing a trait for that type 
     let (impl_generics, ty_generics, _) = input.generics.split_for_impl();
 
-    #[cfg(feature = "serialize")]
-    {
-        let mut where_clause = where_clause.cloned().unwrap_or_else(|| parse_quote!(where));
-        where_clause.predicates.push(parse_quote!(Self: 'static));
-
-        output.extend(quote! {
-            impl #impl_generics ::rumpsteak::serialize::SerializeChoices for #ident #ty_generics #where_clause {
-                fn serialize_choices(mut s: ::rumpsteak::serialize::ChoicesSerializer<'_>) {
-                    #(s.serialize_choice::<#labels, #tys>();)*
-                }
-            }
-        });
-    }
-
+    // Extend where clauses with related impl
     let mut generics = input.generics.clone();
     generics.make_where_clause().predicates.push(parse_quote! {
         __R::Message: #(::rumpsteak::Message<#labels> +)*
@@ -198,6 +220,7 @@ fn session_enum(mut input: ItemEnum) -> Result<TokenStream> {
     Ok(quote!(#input #output))
 }
 
+/// Maych given timed session with either a type of an enum
 pub fn session_timed(attr: TokenStream, input: TokenStream) -> Result<TokenStream> {
     let Nothing = parse2(attr)?;
     match parse2::<Item>(input)? {
