ec38f62da0df19d11f782533457ccf3d409b4325
Nicolas <lagaillardie.nicolas@live.fr>
2023-06-15 12:42:27 +0100

    fixed scripts

--- scripts/benches_runtime_mesh_ring_ping_pong.sh
+++ scripts/benches_runtime_mesh_ring_ping_pong.sh
@@ -11,6 +11,7 @@ date
 
 # Create folders if they do not exist
 mkdir -p save/
+rm -rf save/*
 
 # Run all mesh benchmarks
 echo "Mesh full bench"
@@ -18,6 +19,11 @@ date
 mkdir -p save/mesh/
 cargo bench --bench="mesh_*" --all-features -- --verbose
 find . -name "*.svg" -delete
+find target/ -name "raw.csv" -delete
+find target/ -name "benchmark.json" -delete
+find target/ -name "tukey.json" -delete
+find target/ -name "index.html" -delete
+find target/ -name "sample.json" -delete
 mv -f target/criterion/* save/mesh/
 echo "Mesh full bench weight"
 du -s -m
@@ -29,21 +35,28 @@ date
 mkdir -p save/ring/
 cargo bench --bench="ring_*" --all-features -- --verbose
 find . -name "*.svg" -delete
+find target/ -name "raw.csv" -delete
+find target/ -name "benchmark.json" -delete
+find target/ -name "tukey.json" -delete
+find target/ -name "index.html" -delete
+find target/ -name "sample.json" -delete
 mv -f target/criterion/* save/ring/
 echo "Ring full bench weight"
 du -s -m
 cargo clean
 
-# Run all ring benchmarks
+# Run all ping-pong benchmarks
 echo "Ping-pong full bench"
 date
 mkdir -p save/ping_pong/
 cargo bench --bench="ping_pong_*" --all-features -- --verbose
 find . -name "*.svg" -delete
+find target/ -name "raw.csv" -delete
+find target/ -name "benchmark.json" -delete
+find target/ -name "tukey.json" -delete
+find target/ -name "index.html" -delete
+find target/ -name "sample.json" -delete
 mv -f target/criterion/* save/ping_pong/
 echo "Ping-pong full bench weight"
 du -s -m
 cargo clean
-
-## Concatenate all results in the results/ping_pong_mesh_ring.csv file
-# python3 scripts/create_graphs/timed_affine_mesh_ring.py
