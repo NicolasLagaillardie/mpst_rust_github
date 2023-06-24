c00029255df1eab660d49680e298818d68b4d50e
Lag <lagaillardie.nicolas@live.fr>
2023-06-24 15:00:58 +0200

    fixed toml

--- benches/examples_all_interleaved/basic/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod circuit_breaker_solo;
-pub mod logging_solo;
--- benches/mesh_all/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod baking;
-pub mod baking_cancel_inline;
-pub mod cancel;
-pub mod cancel_broadcast;
-pub mod empty;
-pub mod normal;
-pub mod timed_baking_cancel;
