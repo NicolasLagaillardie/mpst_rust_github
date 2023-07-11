801a8dd13fe2ee039213c161647f88866b9828d2
Lag <lagaillardie.nicolas@live.fr>
2023-07-11 19:37:35 +0200

    updated script to fit thesis

--- scripts/examples_affine_timed_literature.sh
+++ scripts/examples_affine_timed_literature.sh
@@ -5,62 +5,67 @@
 # Stop upon any error
 set -e
 
-## Compile basic examples
-bash ./scripts/create_files/compile_full.sh circuit_breaker 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh logging 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh circuit_breaker_baking 10 baking
-bash ./scripts/create_files/compile_full.sh logging_baking 10 baking
-bash ./scripts/create_files/compile_full.sh distributed_calc 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh dns_fowler 10 baking
-bash ./scripts/create_files/compile_full.sh dns_fowler_checking 10 baking_checking
-bash ./scripts/create_files/compile_full.sh dns_imai 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh o_auth 10 baking
-bash ./scripts/create_files/compile_full.sh o_auth_checking 10 baking_checking
-bash ./scripts/create_files/compile_full.sh o_auth_transport 10 transport_macros_multiple
-bash ./scripts/create_files/compile_full.sh online_wallet 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh simple_voting 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh smtp 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh three_buyers 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh three_travel 10 macros_multiple
-bash ./scripts/create_files/compile_full.sh video_stream_basic 10 macros_simple
-bash ./scripts/create_files/compile_full.sh servo 10 baking
-bash ./scripts/create_files/compile_full.sh remote_data 10 baking
-bash ./scripts/create_files/compile_full.sh http 10 baking
+## Compile baking examples
+bash ./scripts/create_files/compile_full.sh fib 1 baking
+bash ./scripts/create_files/compile_full.sh simple_voting 1 baking
+bash ./scripts/create_files/compile_full.sh distributed_calc 1 baking
+bash ./scripts/create_files/compile_full.sh three_buyers 1 baking
+bash ./scripts/create_files/compile_full.sh three_travel 1 baking
+bash ./scripts/create_files/compile_full.sh o_auth 1 baking
+bash ./scripts/create_files/compile_full.sh online_wallet 1 baking
+bash ./scripts/create_files/compile_full.sh smtp 1 baking
+bash ./scripts/create_files/compile_full.sh servo 1 baking
+bash ./scripts/create_files/compile_full.sh logging 1 baking
+bash ./scripts/create_files/compile_full.sh video_stream 1 baking
+bash ./scripts/create_files/compile_full.sh remote_data 1 baking
+bash ./scripts/create_files/compile_full.sh circuit_breaker 1 baking
 
-## Compile checking examples
-bash ./scripts/create_files/compile_full.sh video_stream 10 baking_checking
-bash ./scripts/create_files/compile_full.sh adder 10 baking_checking
-bash ./scripts/create_files/compile_full.sh basic 10 baking_checking
-bash ./scripts/create_files/compile_full.sh servo_8257_original 10 baking_checking
-bash ./scripts/create_files/compile_full.sh servo_8257_fixed 10 baking_checking
+# ## Compile extras examples
+# bash ./scripts/create_files/compile_full.sh o_auth_checking 1 baking_checking
+# bash ./scripts/create_files/compile_full.sh o_auth_transport 1 transport_macros_multiple
+# bash ./scripts/create_files/compile_full.sh dns_fowler 1 baking
+# bash ./scripts/create_files/compile_full.sh dns_fowler_checking 1 baking_checking
+# bash ./scripts/create_files/compile_full.sh dns_imai 1 baking
+# bash ./scripts/create_files/compile_full.sh http 1 baking
 
-## Compile interleaved examples
-bash ./scripts/create_files/compile_full.sh circuit_breaker_logging 10 interleaved
-bash ./scripts/create_files/compile_full.sh circuit_breaker_solo 10 interleaved
-bash ./scripts/create_files/compile_full.sh logging_solo 10 interleaved
-bash ./scripts/create_files/compile_full.sh sleeping_barber 10 interleaved
+# ## Compile checking examples
+# bash ./scripts/create_files/compile_full.sh video_stream 1 baking_checking
+# bash ./scripts/create_files/compile_full.sh adder 1 baking_checking
+# bash ./scripts/create_files/compile_full.sh basic 1 baking_checking
+# bash ./scripts/create_files/compile_full.sh servo_8257_original 1 baking_checking
+# bash ./scripts/create_files/compile_full.sh servo_8257_fixed 1 baking_checking
+
+# ## Compile interleaved examples
+# bash ./scripts/create_files/compile_full.sh circuit_breaker_logging 1 interleaved
+# bash ./scripts/create_files/compile_full.sh circuit_breaker_solo 1 interleaved
+# bash ./scripts/create_files/compile_full.sh logging_solo 1 interleaved
+# bash ./scripts/create_files/compile_full.sh sleeping_barber 1 interleaved
 
 ## Compile timed examples
-bash ./scripts/create_files/compile_full.sh servo_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh servo_8257_async_failing 10 baking_timed
-bash ./scripts/create_files/compile_full.sh servo_8257_async_working 10 baking_timed
-bash ./scripts/create_files/compile_full.sh servo_8257_async_fixed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh simple_voting_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh distributed_calc_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh o_auth_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh online_wallet_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh smtp_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh three_buyers_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh three_travel_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh remote_data_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh http_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh non_feasible 10 baking_timed
-bash ./scripts/create_files/compile_full.sh circuit_breaker_timed 10 baking_timed
-bash ./scripts/create_files/compile_full.sh logging_timed 10 baking_timed
+bash ./scripts/create_files/compile_full.sh fib_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh simple_voting_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh distributed_calc_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh three_buyers_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh three_travel_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh o_auth_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh online_wallet_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh smtp_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh servo_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh logging_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh video_stream_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh remote_data_timed 1 baking_timed
+bash ./scripts/create_files/compile_full.sh circuit_breaker_timed 1 baking_timed
 
-## Compile timed interleaved examples
-bash ./scripts/create_files/compile_full.sh sleeping_barber_timed 10 timed_interleaved
-bash ./scripts/create_files/compile_full.sh circuit_breaker_logging_timed 10 timed_interleaved
+# ## Compile timed extras examples
+# bash ./scripts/create_files/compile_full.sh servo_8257_async_failing 1 baking_timed
+# bash ./scripts/create_files/compile_full.sh servo_8257_async_working 1 baking_timed
+# bash ./scripts/create_files/compile_full.sh servo_8257_async_fixed 1 baking_timed
+# bash ./scripts/create_files/compile_full.sh http_timed 1 baking_timed
+# bash ./scripts/create_files/compile_full.sh non_feasible 1 baking_timed
+
+# ## Compile timed interleaved examples
+# bash ./scripts/create_files/compile_full.sh sleeping_barber_timed 1 timed_interleaved
+# bash ./scripts/create_files/compile_full.sh circuit_breaker_logging_timed 1 timed_interleaved
 
 ## Run benchmarks
 cargo clean
