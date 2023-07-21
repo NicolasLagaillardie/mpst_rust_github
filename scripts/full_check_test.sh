d578e560c5ce57bb5952b660dd7b8b80bf69855e
Lag <lagaillardie.nicolas@live.fr>
2023-07-21 14:49:49 +0200

    updated scripts

--- scripts/full_check_test.sh
+++ scripts/full_check_test.sh
@@ -7,6 +7,10 @@ set -e
 rm -rf cfsm/
 ./scripts/clean_all.sh
 
+# Replace toml
+cat Cargo.toml > scripts/toml/save_cargo.toml
+cat scripts/toml/full_cargo.toml > Cargo.toml
+
 # cargo check each feature
 sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
 grep -iE '[a-z|_]+ =' | \
@@ -27,5 +31,8 @@ while read -r line ; do
     cargo test --all-targets --verbose --workspace --features="$line"
 done
 cargo test --all-targets --verbose --workspace --all-features
+
+# Reverse toml
+cat scripts/toml/save_cargo.toml > Cargo.toml
 
 echo "done"
