4a48e451f86aa4e35bdc5eacb3758d47ddbed02f
Lag <lagaillardie.nicolas@live.fr>
2023-07-21 14:29:35 +0200

    can now test all featuresgit add . with only 'one' commandgit add .!

--- scripts/full_check.sh
+++ scripts/full_check.sh
@@ -7,12 +7,18 @@ set -e
 rm -rf cfsm/
 ./scripts/clean_all.sh
 
-sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | grep -iE '[a-z|_]+ =' | grep -iEwo '[a-z|_]+'
+sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
+grep -iE '[a-z|_]+ =' | \
+grep -iEwo '[a-z|_]+ ' | \
+while read -r line ; do
+    echo "Processing $line"
+    cargo check --lib --verbose --workspace --features="$line"
+done
 
 
 
 
-cargo check --lib --verbose --workspace --features="macros_multiple checking"
+
 RUST_BACKTRACE=1 cargo check --all-targets --verbose --workspace --all-features
 cargo test --verbose --workspace --no-default-features --no-run
 cargo test --all-targets --workspace --all-features
