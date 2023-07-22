fc343926061689dda7d2b73ea2a5f0a772979ae2
Lag <lagaillardie.nicolas@live.fr>
2023-07-22 22:07:49 +0200

    updated workflow

--- scripts/partial_check_test.sh
+++ scripts/partial_check_test.sh
@@ -5,33 +5,33 @@
 set -e
 
 rm -rf cfsm/
-./scripts/clean_all.sh
+bash ./scripts/clean_all.sh
 
 # Replace toml
 cat Cargo.toml > scripts/toml/save_cargo.toml
 cat scripts/toml/full_cargo.toml > Cargo.toml
 
 # cargo check each feature
-cargo check --all-targets --verbose --workspace --no-default-features
+cargo check --all-targets --verbose --workspace --no-default-features -- --skip kmc --skip transport_udp
 sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
 grep -iE '[a-z|_]+ =' | \
 grep -iEwo '[a-z|_]+ ' | \
 while read -r line ; do
     echo "Processing $line"
-    RUSTFLAGS="-Z macro-backtrace" cargo check --all-targets --verbose --workspace --features="$line"
+    RUSTFLAGS="-Z macro-backtrace" cargo check --all-targets --verbose --workspace --features="$line" -- --skip kmc --skip transport_udp
 done
-RUSTFLAGS="-Z macro-backtrace" cargo check --all-targets --verbose --workspace --all-features
+RUSTFLAGS="-Z macro-backtrace" cargo check --all-targets --verbose --workspace --all-features -- --skip kmc --skip transport_udp
 
 # cargo test each feature
-cargo test --all-targets --verbose --workspace --no-default-features
+cargo test --all-targets --verbose --workspace --no-default-features -- --skip kmc --skip transport_udp
 sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
 grep -iE '[a-z|_]+ =' | \
 grep -iEwo '[a-z|_]+ ' | \
 while read -r line ; do
     echo "Processing $line"
-    RUSTFLAGS="-Z macro-backtrace" cargo test --all-targets --verbose --workspace --features="$line"
+    RUSTFLAGS="-Z macro-backtrace" cargo test --all-targets --verbose --workspace --features="$line" -- --skip kmc --skip transport_udp
 done
-RUSTFLAGS="-Z macro-backtrace" cargo test --all-targets --verbose --workspace --all-features
+RUSTFLAGS="-Z macro-backtrace" cargo test --all-targets --verbose --workspace --all-features -- --skip kmc --skip transport_udp
 
 # Reverse toml
 cat scripts/toml/save_cargo.toml > Cargo.toml
