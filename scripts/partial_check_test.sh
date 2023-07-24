5a1c8b529f2fe5070918b8e90633c1b585298880
Lag <lagaillardie.nicolas@live.fr>
2023-07-24 22:30:56 +0200

    removed backtrace from partial_check_test

--- scripts/partial_check_test.sh
+++ scripts/partial_check_test.sh
@@ -14,9 +14,9 @@ grep -iE '[a-z|_]+ =' | \
 grep -iEwo '[a-z|_]+ ' | \
 while read -r line ; do
     echo "Processing $line"
-    RUSTFLAGS="-Z macro-backtrace" cargo check --all-targets --verbose --workspace --features="$line"
+    cargo check --all-targets --verbose --workspace --features="$line"
 done
-RUSTFLAGS="-Z macro-backtrace" cargo check --all-targets --verbose --workspace --all-features
+cargo check --all-targets --verbose --workspace --all-features
 
 # cargo test each feature
 cargo test --all-targets --verbose --workspace --no-default-features -- --skip kmc --skip transport_udp
@@ -25,9 +25,9 @@ grep -iE '[a-z|_]+ =' | \
 grep -iEwo '[a-z|_]+ ' | \
 while read -r line ; do
     echo "Processing $line"
-    RUSTFLAGS="-Z macro-backtrace" cargo test --all-targets --verbose --workspace --features="$line" -- --skip kmc --skip transport_udp
+    cargo test --all-targets --verbose --workspace --features="$line" -- --skip kmc --skip transport_udp
 done
-RUSTFLAGS="-Z macro-backtrace" cargo test --all-targets --verbose --workspace --all-features -- --skip kmc --skip transport_udp
+cargo test --all-targets --verbose --workspace --all-features -- --skip kmc --skip transport_udp
 
 # Reverse toml
 cat scripts/toml/save_cargo.toml > Cargo.toml
