36665a2914888d8877c55372d26d8b4fe2a79150
Lag <lagaillardie.nicolas@live.fr>
2023-07-26 00:24:56 +0200

    updated scripts to clean after each test

--- scripts/full_check_test.sh
+++ scripts/full_check_test.sh
@@ -12,29 +12,44 @@ set -e
     cat scripts/toml/full_cargo.toml > Cargo.toml
     
     # Clean everything
-    ./scripts/clean_all.sh
+    bash ./scripts/clean_all.sh
 
     # cargo check each feature
     cargo check --all-targets --verbose --workspace --no-default-features
+    # Clean everything
+    ./scripts/clean_all.sh
+
     sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
     grep -iE '[a-z|_]+ =' | \
     grep -iEwo '[a-z|_]+ ' | \
     while read -r line ; do
         echo "Processing $line"
         cargo check --all-targets --verbose --workspace --features="$line"
+        # Clean everything
+    bash ./scripts/clean_all.sh
     done
+
     cargo check --all-targets --verbose --workspace --all-features
+    # Clean everything
+    bash ./scripts/clean_all.sh
 
     # cargo test each feature
     cargo test --all-targets --verbose --workspace --no-default-features
+    # Clean everything
+    bash ./scripts/clean_all.sh
+
     sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
     grep -iE '[a-z|_]+ =' | \
     grep -iEwo '[a-z|_]+ ' | \
     while read -r line ; do
         echo "Processing $line"
         cargo test --all-targets --verbose --workspace --features="$line"
+        # Clean everything
+    bash ./scripts/clean_all.sh
     done
     cargo test --all-targets --verbose --workspace --all-features
+    # Clean everything
+    bash ./scripts/clean_all.sh
 
     # Reverse toml
     cat scripts/toml/save_cargo.toml > Cargo.toml
--- scripts/partial_check_test.sh
+++ scripts/partial_check_test.sh
@@ -16,25 +16,41 @@ set -e
 
     # cargo check each feature
     cargo check --all-targets --verbose --workspace --no-default-features
+    # Clean everything
+    bash ./scripts/clean_all.sh
+
     sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
     grep -iE '[a-z|_]+ =' | \
     grep -iEwo '[a-z|_]+ ' | \
     while read -r line ; do
         echo "Processing $line"
         cargo check --all-targets --verbose --workspace --features="$line"
+        # Clean everything
+        bash ./scripts/clean_all.sh
     done
+
     cargo check --all-targets --verbose --workspace --all-features
+    # Clean everything
+    bash ./scripts/clean_all.sh
 
     # cargo test each feature
     cargo test --all-targets --verbose --workspace --no-default-features -- --skip kmc --skip transport_udp
+    # Clean everything
+    bash ./scripts/clean_all.sh
+
     sed -n '/^################################### Features$/,/^################################### Doc handling for all-features$/p' Cargo.toml | \
     grep -iE '[a-z|_]+ =' | \
     grep -iEwo '[a-z|_]+ ' | \
     while read -r line ; do
         echo "Processing $line"
         cargo test --all-targets --verbose --workspace --features="$line" -- --skip kmc --skip transport_udp
+        # Clean everything
+        bash ./scripts/clean_all.sh
     done
+    
     cargo test --all-targets --verbose --workspace --all-features -- --skip kmc --skip transport_udp
+    # Clean everything
+    bash ./scripts/clean_all.sh
 
     # Reverse toml
     cat scripts/toml/save_cargo.toml > Cargo.toml
