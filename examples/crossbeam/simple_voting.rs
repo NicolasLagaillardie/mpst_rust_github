11cb339a94fea5583755c083282e266a07e08fc2
Lag <lagaillardie.nicolas@live.fr>
2023-07-26 19:25:59 +0200

    added simple voting crossbeam

--- examples/crossbeam/simple_voting.rs
+++ examples/crossbeam/simple_voting.rs
@@ -1,104 +1,42 @@
-#![allow(clippy::type_complexity, dead_code)]
+#![allow(clippy::type_complexity)]
 
-use mpstthree::binary::close::close;
-use mpstthree::binary::fork::fork_with_thread_id;
-use mpstthree::binary::recv::recv;
-use mpstthree::binary::send::send;
-use mpstthree::binary::struct_trait::{end::End, recv::Recv, send::Send, session::Session};
-use mpstthree::{choose, offer};
-
-use rand::{thread_rng, Rng};
-
-use std::error::Error;
 use std::thread::spawn;
 
-// S
-enum Binary0A {
-    Accept(Recv<(), Rec1A>),
-    // Not used
-    Reject(Recv<(), End>),
-}
-enum Binary1A {
-    Yes(Recv<(), End>),
-    No(Recv<(), End>),
-}
-type Rec0A = Recv<Binary0A, End>;
-type Rec1A = Recv<Binary1A, End>;
-type FullA = Recv<(), Rec0A>;
+use crossbeam_channel::{bounded, Receiver};
 
-fn binary_a(s: FullA) -> Result<(), Box<dyn Error>> {
-    let (_authenticate, s) = recv(s)?;
-
-    offer!(s, {
-        Binary0A::Accept(s) => {
-            let (_ok, s) = recv(s)?;
-
-            offer!(s, {
-                Binary1A::Yes(s) => {
-                    let (_yes, s) = recv(s)?;
-                    close(s)
-                },
-                Binary1A::No(s) => {
-                    let (_no, s) = recv(s)?;
-                    close(s)
-                },
-            })
-        },
-        Binary0A::Reject(s) => {
-            let (_reject, s) = recv(s)?;
-            close(s)
-        },
-    })
-}
-
-// C
-type FullB = <FullA as Session>::Dual;
-
-fn binary_yes_b(s: FullB) -> Result<(), Box<dyn Error>> {
-    let s = send((), s);
-    let s = choose!(Binary0A::Accept, s);
-    let s: Send<Binary1A, End> = send((), s);
-    let s = choose!(Binary1A::Yes, s);
-    let s = send((), s);
-    close(s)
-}
-
-fn binary_no_b(s: FullB) -> Result<(), Box<dyn Error>> {
-    let s = send((), s);
-    let s = choose!(Binary0A::Accept, s);
-    let s: Send<Binary1A, End> = send((), s);
-    let s = choose!(Binary1A::No, s);
-    let s = send((), s);
-    close(s)
-}
-
-// Not used
-fn binary_reject_b(s: FullB) -> Result<(), Box<dyn Error>> {
-    let s = send((), s);
-    let s = choose!(Binary0A::Reject, s);
-    let s = send((), s);
-    close(s)
-}
+type S0 = Receiver<Receiver<Receiver<Receiver<Receiver<()>>>>>;
+type S1 = Receiver<Receiver<Receiver<Receiver<()>>>>;
+type S2 = Receiver<Receiver<Receiver<()>>>;
+type S3 = Receiver<Receiver<()>>;
+type S4 = Receiver<()>;
+type S5 = ();
 
 fn main() {
-    let mut threads = Vec::new();
-    let mut sessions = Vec::new();
-
-    let (thread, session) = fork_with_thread_id(binary_a);
-
-    threads.push(thread);
-    sessions.push(session);
-
     let main = spawn(move || {
-        let choice = thread_rng().gen_range(1..=2);
+        let (sender_s_0, receiver_s_0) = bounded::<S0>(1);
+        let (sender_s_1, receiver_s_1) = bounded::<S1>(1);
+        let (sender_s_2, receiver_s_2) = bounded::<S2>(1);
+        let (sender_s_3, receiver_s_3) = bounded::<S3>(1);
+        let (sender_s_4, receiver_s_4) = bounded::<S4>(1);
+        let (sender_s_5, receiver_s_5) = bounded::<S5>(1);
 
-        if choice != 1 {
-            sessions.into_iter().for_each(|s| binary_yes_b(s).unwrap());
-        } else {
-            sessions.into_iter().for_each(|s| binary_no_b(s).unwrap());
-        }
+        sender_s_0.send(receiver_s_1).unwrap();
+        let receiver_s_1_bis = receiver_s_0.recv().unwrap();
 
-        threads.into_iter().for_each(|elt| elt.join().unwrap());
+        sender_s_1.send(receiver_s_2).unwrap();
+        let receiver_s_2_bis = receiver_s_1_bis.recv().unwrap();
+
+        sender_s_2.send(receiver_s_3).unwrap();
+        let receiver_s_3_bis = receiver_s_2_bis.recv().unwrap();
+
+        sender_s_3.send(receiver_s_4).unwrap();
+        let receiver_s_4_bis = receiver_s_3_bis.recv().unwrap();
+
+        sender_s_4.send(receiver_s_5).unwrap();
+        let receiver_s_5_bis = receiver_s_4_bis.recv().unwrap();
+
+        sender_s_5.send(()).unwrap();
+        receiver_s_5_bis.recv().unwrap();
     });
 
     main.join().unwrap();
