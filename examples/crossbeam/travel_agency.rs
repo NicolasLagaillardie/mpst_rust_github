51afba48c45d1b55974bae956e887f9f7c25e045
Lag <lagaillardie.nicolas@live.fr>
2023-07-26 19:31:12 +0200

    added travel agency crossbeam

--- examples/crossbeam/travel_agency.rs
+++ examples/crossbeam/travel_agency.rs
@@ -1,121 +1,47 @@
 #![allow(clippy::type_complexity)]
 
-use mpstthree::binary::close::close;
-use mpstthree::binary::fork::fork_with_thread_id;
-use mpstthree::binary::recv::recv;
-use mpstthree::binary::send::send;
-use mpstthree::binary::struct_trait::{end::End, recv::Recv, send::Send, session::Session};
-use mpstthree::{choose, offer};
-
-use rand::{thread_rng, Rng};
-
-use std::error::Error;
 use std::thread::spawn;
 
-// S
-enum Binary0A {
-    Accept(Rec1A),
-    Query(Recv<(), Recv<(), Recv<(), Rec0A>>>),
-}
-enum Binary1A {
-    Yes(Recv<(), Recv<(), Recv<(), Recv<(), Recv<(), End>>>>>),
-    No(Recv<(), Recv<(), Recv<(), End>>>),
-}
-type Rec0A = Recv<Binary0A, End>;
-type Rec1A = Recv<Binary1A, End>;
-type FullA = Rec0A;
+use crossbeam_channel::{bounded, Receiver};
 
-fn binary_a(s: FullA) -> Result<(), Box<dyn Error>> {
-    offer!(s, {
-        Binary0A::Accept(s) => {
-            offer!(s, {
-                Binary1A::Yes(s) => {
-                    let (_yes_a, s) = recv(s)?;
-                    let (_yes_s, s) = recv(s)?;
-                    let (_payment, s) = recv(s)?;
-                    let (_ack, s) = recv(s)?;
-                    let (_bye, s) = recv(s)?;
-                    close(s)
-                },
-                Binary1A::No(s) => {
-                    let (_no_a, s) = recv(s)?;
-                    let (_no_s, s) = recv(s)?;
-                    let (_bye_a, s) = recv(s)?;
-                    close(s)
-                },
-            })
-        },
-        Binary0A::Query(s) => {
-            let (_query_a, s) = recv(s)?;
-            let (_quote_c, s) = recv(s)?;
-            let (_dummy_s, s) = recv(s)?;
-            binary_a(s)
-        },
-    })
-}
-
-// C
-type FullB = <FullA as Session>::Dual;
-
-fn binary_yes_b(s: FullB) -> Result<(), Box<dyn Error>> {
-    let s: Send<Binary1A, End> =
-        choose!(Binary0A::Accept, s);
-    let s = choose!(Binary1A::Yes, s);
-    let s = send((), s);
-    let s = send((), s);
-    let s = send((), s);
-    let s = send((), s);
-    let s = send((), s);
-    close(s)
-}
-
-fn binary_no_b(s: FullB) -> Result<(), Box<dyn Error>> {
-    let s: Send<Binary1A, End> =
-        choose!(Binary0A::Accept, s);
-    let s = choose!(Binary1A::No, s);
-    let s = send((), s);
-    let s = send((), s);
-    let s = send((), s);
-    close(s)
-}
-
-fn binary_query_b(s: FullB) -> Result<FullB, Box<dyn Error>> {
-    let s = choose!(Binary0A::Query, s);
-    let s = send((), s);
-    let s = send((), s);
-    let s = send((), s);
-    Ok(s)
-}
+type S0 = Receiver<Receiver<Receiver<Receiver<Receiver<()>>>>>;
+type S1 = Receiver<Receiver<Receiver<Receiver<()>>>>;
+type S2 = Receiver<Receiver<Receiver<()>>>;
+type S3 = Receiver<Receiver<()>>;
+type S4 = Receiver<()>;
+type S5 = ();
 
 fn main() {
-    let mut threads = Vec::new();
-    let mut sessions = Vec::new();
-
-    let (thread, session) = fork_with_thread_id(binary_a);
-
-    threads.push(thread);
-    sessions.push(session);
-
     let main = spawn(move || {
         for _ in 0..LOOPS {
-            sessions = sessions
-                .into_iter()
-                .map(|s| binary_query_b(s).unwrap())
-                .collect::<Vec<_>>();
+            let (sender_s_0, receiver_s_0) = bounded::<S0>(1);
+            let (sender_s_1, receiver_s_1) = bounded::<S1>(1);
+            let (sender_s_2, receiver_s_2) = bounded::<S2>(1);
+            let (sender_s_3, receiver_s_3) = bounded::<S3>(1);
+            let (sender_s_4, receiver_s_4) = bounded::<S4>(1);
+            let (sender_s_5, receiver_s_5) = bounded::<S5>(1);
+
+            sender_s_0.send(receiver_s_1).unwrap();
+            let receiver_s_1_bis = receiver_s_0.recv().unwrap();
+
+            sender_s_1.send(receiver_s_2).unwrap();
+            let receiver_s_2_bis = receiver_s_1_bis.recv().unwrap();
+
+            sender_s_2.send(receiver_s_3).unwrap();
+            let receiver_s_3_bis = receiver_s_2_bis.recv().unwrap();
+
+            sender_s_3.send(receiver_s_4).unwrap();
+            let receiver_s_4_bis = receiver_s_3_bis.recv().unwrap();
+
+            sender_s_4.send(receiver_s_5).unwrap();
+            let receiver_s_5_bis = receiver_s_4_bis.recv().unwrap();
+
+            sender_s_5.send(()).unwrap();
+            receiver_s_5_bis.recv().unwrap();
         }
-
-        let choice = thread_rng().gen_range(1..=2);
-
-        if choice != 1 {
-            sessions.into_iter().for_each(|s| binary_yes_b(s).unwrap());
-        } else {
-            sessions.into_iter().for_each(|s| binary_no_b(s).unwrap());
-        }
-
-        threads.into_iter().for_each(|elt| elt.join().unwrap());
     });
 
     main.join().unwrap();
 }
 
-static LOOPS: i32 = 100;
+static LOOPS: i64 = 100;
