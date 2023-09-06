79f6e1e9a9a7f4b2290b7cf4c24be1954365062d
Lag <lagaillardie.nicolas@live.fr>
2023-09-06 21:02:59 +0200

    updated ping pong benches

--- benches/ping_pong_save/ping_pong_mpst_1.rs
+++ benches/ping_pong_save/ping_pong_mpst_1.rs
@@ -136,7 +136,7 @@ pub fn ping_pong_protocol_mpst(c: &mut Criterion) {
 
 criterion_group! {
     name = bench;
-    config = Criterion::default().significance_level(0.05).without_plots().sample_size(10000);
+    config = Criterion::default().significance_level(0.05).without_plots().sample_size(50000);
     targets = ping_pong_protocol_mpst
 }
 
