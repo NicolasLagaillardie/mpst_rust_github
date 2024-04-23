fbe0c5c20925d384e27ebd9974ac2faa1dc6ea00
Lag <nicolas.lagaillardie@tuta.com>
2024-04-24 01:16:26 +0200

    push for Docker

--- scripts/anon/sh/full_benches_compile_mesh_ring.sh
+++ scripts/anon/sh/full_benches_compile_mesh_ring.sh
@@ -14,73 +14,73 @@ date
 ## Mesh
 ### Two
 echo "Mesh two"
-bash ./scripts/create_files/compile_normal.sh mesh_two_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh mesh_two_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh mesh_two_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh mesh_two_baking_atmp 1 baking_atmp
 
 ### Three
 echo "Mesh three"
-bash ./scripts/create_files/compile_normal.sh mesh_three_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh mesh_three_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh mesh_three_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh mesh_three_baking_atmp 1 baking_atmp
 
 ### Four
 echo "Mesh four"
-bash ./scripts/create_files/compile_normal.sh mesh_four_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh mesh_four_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh mesh_four_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh mesh_four_baking_atmp 1 baking_atmp
 
 ### Five
 echo "Mesh five"
-bash ./scripts/create_files/compile_normal.sh mesh_five_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh mesh_five_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh mesh_five_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh mesh_five_baking_atmp 1 baking_atmp
 
 ### Six
 echo "Mesh six"
-bash ./scripts/create_files/compile_normal.sh mesh_six_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh mesh_six_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh mesh_six_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh mesh_six_baking_atmp 1 baking_atmp
 
 ### Seven
 echo "Mesh seven"
-bash ./scripts/create_files/compile_normal.sh mesh_seven_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh mesh_seven_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh mesh_seven_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh mesh_seven_baking_atmp 1 baking_atmp
 
 ### Eight
 echo "Mesh eight"
-bash ./scripts/create_files/compile_normal.sh mesh_eight_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh mesh_eight_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh mesh_eight_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh mesh_eight_baking_atmp 1 baking_atmp
 
 ## Ring
 date
 
 ### Two
 echo "Ring two"
-bash ./scripts/create_files/compile_normal.sh ring_two_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh ring_two_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh ring_two_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh ring_two_baking_atmp 1 baking_atmp
 
 ### Three
 echo "Ring three"
-bash ./scripts/create_files/compile_normal.sh ring_three_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh ring_three_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh ring_three_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh ring_three_baking_atmp 1 baking_atmp
 
 ### Four
 echo "Ring four"
-bash ./scripts/create_files/compile_normal.sh ring_four_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh ring_four_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh ring_four_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh ring_four_baking_atmp 1 baking_atmp
 
 ### Five
 echo "Ring five"
-bash ./scripts/create_files/compile_normal.sh ring_five_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh ring_five_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh ring_five_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh ring_five_baking_atmp 1 baking_atmp
 
 ### Six
 echo "Ring six"
-bash ./scripts/create_files/compile_normal.sh ring_six_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh ring_six_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh ring_six_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh ring_six_baking_atmp 1 baking_atmp
 
 ### Seven
 echo "Ring seven"
-bash ./scripts/create_files/compile_normal.sh ring_seven_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh ring_seven_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh ring_seven_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh ring_seven_baking_atmp 1 baking_atmp
 
 ### Eight
 echo "Ring eight"
-bash ./scripts/create_files/compile_normal.sh ring_eight_baking_ampst 100 baking
-bash ./scripts/create_files/compile_normal.sh ring_eight_baking_atmp 100 baking_atmp
+bash ./scripts/create_files/compile_normal.sh ring_eight_baking_ampst 1 baking
+bash ./scripts/create_files/compile_normal.sh ring_eight_baking_atmp 1 baking_atmp
