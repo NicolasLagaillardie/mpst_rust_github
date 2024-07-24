55f3ad11dd4fce743e280b23b2be82950bda8597
Lag <nicolas.lagaillardie@tuta.com>
2024-07-24 23:12:22 +0200

    Fixing fail_3.stderr test

--- tests/nuscr_mod/correct/Basic.rs
+++ tests/nuscr_mod/correct/Basic.rs
@@ -11,16 +11,28 @@ use std::time::Instant;
 generate_atmp!(MeshedChannels, A, B, C);
 
 // Types of the payloads
-struct Integer;
 struct Stri;
+struct Integer;
 struct Payload;
-struct Test2 { payload: Integer }
 struct Test5 { payload: Payload }
+struct Test2 { payload: Integer }
 struct Test1;
 struct Test3 { payload: Stri }
 struct Test4 { payload: Payload }
 
 // Binary sessions in depth 0
+// Binary sessions for B
+type Message_0_v_0_FromBToC = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromBToC>;
+type Message_0_v_1_FromBToC = End;
+type Message_0_v_0_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromBToA>;
+type Message_0_v_1_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_2_FromBToA>;
+type Message_0_v_2_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_3_FromBToA>;
+type Message_0_v_3_FromBToA = RecvTimed<Test5, 'a', 0, true, 2, true, ' ', Message_0_v_4_FromBToA>;
+type Message_0_v_4_FromBToA = RecvTimed<Test2, 'a', 0, true, 2, true, ' ', Message_0_v_5_FromBToA>;
+type Message_0_v_5_FromBToA = RecvTimed<Test3, 'a', 0, true, 2, true, ' ', Message_0_v_6_FromBToA>;
+type Message_0_v_6_FromBToA = RecvTimed<Test4, 'a', 0, true, 2, true, ' ', Message_0_v_7_FromBToA>;
+type Message_0_v_7_FromBToA = End;
+
 // Binary sessions for A
 type Message_0_v_0_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromAToB>;
 type Message_0_v_1_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_2_FromAToB>;
@@ -37,23 +49,7 @@ type Message_0_v_0_FromCToA = End;
 type Message_0_v_0_FromCToB = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromCToB>;
 type Message_0_v_1_FromCToB = End;
 
-// Binary sessions for B
-type Message_0_v_0_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromBToA>;
-type Message_0_v_1_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_2_FromBToA>;
-type Message_0_v_2_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_3_FromBToA>;
-type Message_0_v_3_FromBToA = RecvTimed<Test5, 'a', 0, true, 2, true, ' ', Message_0_v_4_FromBToA>;
-type Message_0_v_4_FromBToA = RecvTimed<Test2, 'a', 0, true, 2, true, ' ', Message_0_v_5_FromBToA>;
-type Message_0_v_5_FromBToA = RecvTimed<Test3, 'a', 0, true, 2, true, ' ', Message_0_v_6_FromBToA>;
-type Message_0_v_6_FromBToA = RecvTimed<Test4, 'a', 0, true, 2, true, ' ', Message_0_v_7_FromBToA>;
-type Message_0_v_7_FromBToA = End;
-type Message_0_v_0_FromBToC = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromBToC>;
-type Message_0_v_1_FromBToC = End;
-
 // Stacks in depth 0
-// Stacks for C
-type Ordering_0_v_0_ForC = RoleB<Ordering_0_v_1_ForC>;
-type Ordering_0_v_1_ForC = RoleEnd;
-
 // Stacks for A
 type Ordering_0_v_0_ForA = RoleB<Ordering_0_v_1_ForA>;
 type Ordering_0_v_1_ForA = RoleB<Ordering_0_v_2_ForA>;
@@ -74,6 +70,10 @@ type Ordering_0_v_5_ForB = RoleA<Ordering_0_v_6_ForB>;
 type Ordering_0_v_6_ForB = RoleA<Ordering_0_v_7_ForB>;
 type Ordering_0_v_7_ForB = RoleA<Ordering_0_v_8_ForB>;
 type Ordering_0_v_8_ForB = RoleEnd;
+
+// Stacks for C
+type Ordering_0_v_0_ForC = RoleB<Ordering_0_v_1_ForC>;
+type Ordering_0_v_1_ForC = RoleEnd;
 
 // Endpoints in depth 0
 // Endpoint for role A
