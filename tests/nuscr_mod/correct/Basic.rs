e0f6008ddcef8fffaf4b5b70162e451c0028f3b1
Lag <nicolas.lagaillardie@tuta.com>
2024-09-18 19:41:22 +0200

    Fixing issues
    Signed-off-by: Lag <nicolas.lagaillardie@tuta.com>

--- tests/nuscr_mod/correct/Basic.rs
+++ tests/nuscr_mod/correct/Basic.rs
@@ -11,9 +11,9 @@ use std::time::Instant;
 generate_atmp!(MeshedChannels, A, B, C);
 
 // Types of the payloads
+struct Stri;
 struct Payload;
 struct Integer;
-struct Stri;
 struct Test1;
 struct Test3 { payload: Stri }
 struct Test4 { payload: Payload }
@@ -21,6 +21,22 @@ struct Test5 { payload: Payload }
 struct Test2 { payload: Integer }
 
 // Binary sessions in depth 0
+// Binary sessions for A
+type Message_0_v_0_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromAToB>;
+type Message_0_v_1_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_2_FromAToB>;
+type Message_0_v_2_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_3_FromAToB>;
+type Message_0_v_3_FromAToB = SendTimed<Test5, 'a', 0, true, 2, true, ' ', Message_0_v_4_FromAToB>;
+type Message_0_v_4_FromAToB = SendTimed<Test2, 'a', 0, true, 2, true, ' ', Message_0_v_5_FromAToB>;
+type Message_0_v_5_FromAToB = SendTimed<Test3, 'a', 0, true, 2, true, ' ', Message_0_v_6_FromAToB>;
+type Message_0_v_6_FromAToB = SendTimed<Test4, 'a', 0, true, 2, true, ' ', Message_0_v_7_FromAToB>;
+type Message_0_v_7_FromAToB = End;
+type Message_0_v_0_FromAToC = End;
+
+// Binary sessions for C
+type Message_0_v_0_FromCToA = End;
+type Message_0_v_0_FromCToB = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromCToB>;
+type Message_0_v_1_FromCToB = End;
+
 // Binary sessions for B
 type Message_0_v_0_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromBToA>;
 type Message_0_v_1_FromBToA = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_2_FromBToA>;
@@ -33,27 +49,7 @@ type Message_0_v_7_FromBToA = End;
 type Message_0_v_0_FromBToC = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromBToC>;
 type Message_0_v_1_FromBToC = End;
 
-// Binary sessions for A
-type Message_0_v_0_FromAToC = End;
-type Message_0_v_0_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromAToB>;
-type Message_0_v_1_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_2_FromAToB>;
-type Message_0_v_2_FromAToB = SendTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_3_FromAToB>;
-type Message_0_v_3_FromAToB = SendTimed<Test5, 'a', 0, true, 2, true, ' ', Message_0_v_4_FromAToB>;
-type Message_0_v_4_FromAToB = SendTimed<Test2, 'a', 0, true, 2, true, ' ', Message_0_v_5_FromAToB>;
-type Message_0_v_5_FromAToB = SendTimed<Test3, 'a', 0, true, 2, true, ' ', Message_0_v_6_FromAToB>;
-type Message_0_v_6_FromAToB = SendTimed<Test4, 'a', 0, true, 2, true, ' ', Message_0_v_7_FromAToB>;
-type Message_0_v_7_FromAToB = End;
-
-// Binary sessions for C
-type Message_0_v_0_FromCToA = End;
-type Message_0_v_0_FromCToB = RecvTimed<Test1, 'a', 0, true, 2, true, ' ', Message_0_v_1_FromCToB>;
-type Message_0_v_1_FromCToB = End;
-
 // Stacks in depth 0
-// Stacks for C
-type Ordering_0_v_0_ForC = RoleB<Ordering_0_v_1_ForC>;
-type Ordering_0_v_1_ForC = RoleEnd;
-
 // Stacks for A
 type Ordering_0_v_0_ForA = RoleB<Ordering_0_v_1_ForA>;
 type Ordering_0_v_1_ForA = RoleB<Ordering_0_v_2_ForA>;
@@ -74,6 +70,10 @@ type Ordering_0_v_5_ForB = RoleA<Ordering_0_v_6_ForB>;
 type Ordering_0_v_6_ForB = RoleA<Ordering_0_v_7_ForB>;
 type Ordering_0_v_7_ForB = RoleA<Ordering_0_v_8_ForB>;
 type Ordering_0_v_8_ForB = RoleEnd;
+
+// Stacks for C
+type Ordering_0_v_0_ForC = RoleB<Ordering_0_v_1_ForC>;
+type Ordering_0_v_1_ForC = RoleEnd;
 
 // Endpoints in depth 0
 // Endpoint for role A
